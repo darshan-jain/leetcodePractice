692991959e629e9205ddd996362d4e91bc248d09
Darshan Jain <darshandhokajain@gmail.com>
2024-11-05 10:45:15 -0800

    Time: 2 ms (65.93%) | Memory: 12.3 MB (77.95%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for ind in range(len(nums)):
+            num = nums[ind]
+            if target - num in seen:
+                return [ind,seen[target-num]]
+            seen[num] = ind
+        

f807ad97ca75fb13238f892916a93d37fa546f65
Darshan Jain <darshandhokajain@gmail.com>
2024-11-06 11:02:53 -0800

    Time: 15 ms (38.02%) | Memory: 25.2 MB (68.26%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,13 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        seen = {}
-        for i in range(len(nums)):
-            seen[nums[i]] = 0
-        for num in nums:
-            seen[num] += 1
-            if seen[num] >= 2:
-                return True
+        # Hashmap Approach
+        # seen = {}
+        # for i in range(len(nums)):
+        #     seen[nums[i]] = 0
+        # for num in nums:
+        #     seen[num] += 1
+        #     if seen[num] >= 2:
+        #         return True
             
 
+        # return False
+
+        # set data structure approach 
+        setnums = set()
+        for num in nums:
+            if num in setnums:
+                return True
+            setnums.add(num)
         return False

6bfb01ca5c9ea3942d17979f735f049a3bf231bb
Darshan Jain <darshandhokajain@gmail.com>
2024-11-07 10:36:33 -0800

    Time: 28 ms (84.23%) | Memory: 20.7 MB (34.99%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,11 +6,10 @@ class Solution(object):
         """
         output = [1]
         for i in range(len(nums)-1,0,-1):
-            output.append(output[-1]*nums[i])
+            output.append(output[-1]* nums[i])
         output = output[::-1]
-        left=  1
+        left = 1
         for i in range(len(nums)):
             output[i] = output[i]*left
-            left *= nums[i]
+            left = left* nums[i]
         return output
-        

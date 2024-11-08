d1e6fa78f3326d47720e34b5c39bcbd45ed5c328
Darshan Jain <darshandhokajain@gmail.com>
2024-11-08 11:13:18 -0800

    Time: 3 ms (45.00%) | Memory: 12.3 MB (94.65%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -7,8 +7,8 @@ class Solution(object):
         buy = prices[0]
         profit = 0 
         for price in prices[1:]:
-            if price > buy : 
-                profit += price-buy
-            buy = price 
+            if price > buy:
+                profit += (price - buy)
+            buy = price
         return profit
         

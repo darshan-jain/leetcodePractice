e686c99c275121294067ccb098b62d5eac2f56c4
Darshan Jain <darshandhokajain@gmail.com>
2024-11-09 09:48:24 -0800

    Time: 0 ms (100.00%) | Memory: 11.6 MB (94.95%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -5,10 +5,10 @@ class Solution(object):
         :rtype: int
         """
         left, right = 0, len(nums)-1
-        while left<right:
+        while left< right:
             mid = (left+right)//2
             if nums[mid] < nums[mid+1]:
-                left = mid+1
+                left = mid + 1
             else:
                 right = mid
-        return left
+        return right

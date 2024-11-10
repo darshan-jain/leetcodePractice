8ef57fe9fcb6a74b2eb80df9c0703e0f99375656
Darshan Jain <darshandhokajain@gmail.com>
2024-11-10 10:55:41 -0800

    Time: 501 ms (58.82%) | Memory: 48.6 MB (98.64%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -4,21 +4,22 @@ class Solution(object):
         :type tasks: List[List[int]]
         :rtype: List[int]
         """
-        for i ,t in enumerate(tasks):
+        for i,t in enumerate(tasks):
             t.append(i)
-        tasks.sort(key=lambda t:t[0])
+        tasks.sort(key = lambda t:t[0])
 
         res,minHeap = [],[]
-        i, time = 0, tasks[0][0]
+        i , time =0, tasks[0][0]
 
-        while minHeap or i <len(tasks):
+        while minHeap or i < len(tasks):
             while i < len(tasks) and time >= tasks[i][0]:
-                heapq.heappush(minHeap, [tasks[i][1],tasks[i][2]])
+                heapq.heappush(minHeap,[tasks[i][1],tasks[i][2]])
                 i+=1
             if not minHeap:
                 time = tasks[i][0]
             else:
-                procTime, index = heapq.heappop(minHeap)
-                time +=procTime
+                procTime,index = heapq.heappop(minHeap)
+                time += procTime
                 res.append(index)
         return res
+        

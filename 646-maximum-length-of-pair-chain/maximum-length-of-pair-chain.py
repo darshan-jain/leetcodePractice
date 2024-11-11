58ba825ceb316e4cd719cdb07c4a8ad6a4c1ed0b
Darshan Jain <darshandhokajain@gmail.com>
2024-11-11 13:01:32 -0800

    Time: 5 ms (87.27%) | Memory: 11.9 MB (97.31%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: int
         """
         count = 1
-        pairs.sort(key=lambda x:x[1])
-        
+        pairs.sort(key= lambda x:x[1])
         freetime = pairs[0][1]
+
         for pair in pairs[1:]:
             if pair[0] > freetime:
                 count+=1
                 freetime = pair[1]
-            
         return count
             
         

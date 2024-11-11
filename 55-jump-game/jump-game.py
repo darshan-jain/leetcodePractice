7f8a477beb14f49b1f3e26842dad71e48d9894f0
Darshan Jain <darshandhokajain@gmail.com>
2024-11-11 12:12:35 -0800

    Time: 17 ms (79.60%) | Memory: 12.7 MB (22.03%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,28 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        #Recursive Solution - O(max(nums)^n)
-        # n = len(nums)
-        # def can_reach(i):
-        #     if i==n-1:
-        #         return True
-            
-        #     for jump in range(1,nums[i]+1):
-        #         if can_reach(i+jump):
-        #             return True
-                
-        #     return False 
-
-        # return can_reach(0)
-
-        # Dynamic Programming Approach also exceeds time limit 
-
-        #greedy appraoch 
         n = len(nums)
         target = n-1
         for i in range(n-1,-1,-1):
-            maxjump = nums[i]
-            if i + maxjump >= target:
+            maxJump = nums[i]
+            if i+maxJump >= target:
                 target = i
             
-        return target == 0
+        return target ==0 
+        

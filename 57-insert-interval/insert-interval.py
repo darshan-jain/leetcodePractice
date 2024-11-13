40670118dba818be9f7800449a599782fccf9fbe
Darshan Jain <darshandhokajain@gmail.com>
2024-11-13 10:07:31 -0800

    Time: 4 ms (33.84%) | Memory: 13.3 MB (77.17%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -8,10 +8,11 @@ class Solution(object):
         res = []
         i=0
         n = len(intervals)
-        while(i<n and intervals[i][1]<newInterval[0]):
+
+        while (i<n and intervals[i][1]<newInterval[0]):
             res.append(intervals[i])
             i = i+1
-        while (i<n and intervals[i][0] <= newInterval[1]):
+        while i< n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0],intervals[i][0])
             newInterval[1] = max(newInterval[1],intervals[i][1])
             i=i+1

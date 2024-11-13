823702bf35d65516a07c7014028d5178db3eb76d
Darshan Jain <darshandhokajain@gmail.com>
2024-11-12 20:29:34 -0800

    Time: 0 ms (100.00%) | Memory: 11.7 MB (28.74%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        mask = 0xffffffff
+        while (mask&b)> 0:
+            a,b = a^b,(a&b)<<1
+        return (mask&a) if b >> 0 else a 
+        

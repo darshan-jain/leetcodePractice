3a2a26914fb53170bcbf27d5d7f79ec6fbad1b8c
Darshan Jain <darshandhokajain@gmail.com>
2024-11-14 11:05:17 -0800

    Time: 0 ms (100.00%) | Memory: 11.9 MB (16.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item =='{':
+                stack.append('}')
+            elif item =='[':
+                stack.append(']')
+            elif not stack or stack.pop() != item:
+                return False
+            
+        return not stack
+        

b05b31bf8a96dfb8ea87fd9e69d44efe1a3f8258
Darshan Jain <darshandhokajain@gmail.com>
2024-11-15 10:22:13 -0800

    Time: 0 ms (100.00%) | Memory: 11.5 MB (94.46%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -7,18 +7,19 @@ class Solution(object):
         min =0 
         max =0 
         for i in range(len(s)):
-            if s[i] == '(':
+            if s[i] =='(':
                 min = min+1
                 max = max+1
-            elif s[i] == ')':
+            elif s[i] ==')':
                 min = min-1
                 max = max-1
             else:
                 min = min-1
-                max = max+1
-            if min <0:
+                max=max+1
+            if min<0:
                 min=0
             if max <0:
                 return False
         return min==0
+
         

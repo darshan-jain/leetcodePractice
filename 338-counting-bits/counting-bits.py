763638c64352b4b6f2f5cbff0f6d948cd73b3572
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 13:52:40 -0800

    Time: 7 ms (81.82%) | Memory: 16.8 MB (44.32%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,18 +1,21 @@
 class Solution(object):
-    def helper(self,n):
-        count =0
-        while n:
-            n = n&(n-1)
-            count+=1
-        return count
+    # def helper(self,n):
+    #     count =0
+    #     while n:
+    #         n = n&(n-1)
+    #         count+=1
+    #     return count
     def countBits(self, n):
         """
         :type n: int
         :rtype: List[int]
         """
-        res = []
-        for i in range(n+1):
-            ot = self.helper(i)
-            res.append(ot)
+        # res = []
+        # for i in range(n+1):
+        #     ot = self.helper(i)
+        #     res.append(ot)
+        # return res
+        res = [0]
+        for i in range(1,n+1):
+            res.append(res[i//2]+i%2)
         return res
-        

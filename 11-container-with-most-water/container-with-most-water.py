d6abc1b012596c8caedfba0f2c77aa08bf352ef8
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 12:25:30 -0800

    Time: 91 ms (68.97%) | Memory: 19.8 MB (85.91%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        beg = 0
+        beg = 0 
         end = len(height)-1
-        max_area =0 
-        while beg<end:
+        max_area = 0 
+        while beg < end:
             max_area = max(max_area,min(height[beg],height[end])*(end-beg))
             if height[beg]>height[end]:
                 end = end-1
             else:
-                beg = beg+1
+                beg = beg +1
         return max_area
+        

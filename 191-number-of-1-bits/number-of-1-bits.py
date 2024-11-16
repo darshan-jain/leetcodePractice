213bf6493e67ddc765de070519aa4ab4e5591479
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 13:35:55 -0800

    Time: 1 ms (31.12%) | Memory: 11.6 MB (28.31%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # count = 0
+        # while n:
+        #     count +=n&1
+        #     n = n>>1
+        # return count
+        
         count = 0
         while n:
-            count +=n&1
-            n = n>>1
+            n = n& (n-1)
+            count+=1
         return count
-        

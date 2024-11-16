f78515d0bff99dd75c008eaa5c27023588bc3445
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 12:41:17 -0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (51.00%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left_sum = 0 
+        left_sum =0 
         right_sum = sum(nums)
         for i in range(len(nums)):
-            right_sum -= nums[i]
+            right_sum -=nums[i]
             if left_sum == right_sum:
-                return i 
+                return i
             left_sum += nums[i]
         return -1
-        

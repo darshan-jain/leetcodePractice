c845ece4c13e9afea705cc11197802bd8168b543
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 12:09:51 -0800

    Time: 9 ms (94.80%) | Memory: 13.1 MB (97.59%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -7,23 +7,21 @@ class Solution(object):
         sum = 1
         i=1
         n = len(ratings)
-        while i < n :
-            if ratings[i] == ratings[i-1]:
-                sum = sum+1
-                i +=1
+        while i<n:
+            if ratings[i]==ratings[i-1]:
+                sum = sum +1
+                i = i+1
                 continue
             peak = 1
-            while i< n and ratings[i] > ratings[i-1]:
-                peak +=1
-                sum+=peak
+            while i<n and ratings[i]>ratings[i-1]:
+                peak = peak+1
+                sum = sum+peak
                 i+=1
             down = 1
             while i<n and ratings[i] < ratings[i-1]:
                 sum +=down
-                i+=1
                 down+=1
+                i+=1
             if down > peak:
                 sum += down-peak
-
-        
         return sum

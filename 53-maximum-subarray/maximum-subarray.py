3f4de73c0b2f885a6dd308cc873718b6cb80a43c
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 12:17:47 -0800

    Time: 63 ms (49.86%) | Memory: 21 MB (73.15%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        curr_sum = 0 
         max_sum = nums[0]
-        curr_sum = 0
         for num in nums:
             curr_sum = max(curr_sum+num,num)
             max_sum = max(max_sum,curr_sum)
         return max_sum
+        

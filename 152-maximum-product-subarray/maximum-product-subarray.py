3de0c618f222531217f4499f601b388777a9e2e0
Darshan Jain <darshandhokajain@gmail.com>
2024-11-16 12:20:31 -0800

    Time: 11 ms (30.14%) | Memory: 12.1 MB (45.35%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_p= min_p = nums[0]
-        maximum_product = max_p
+        max_p = min_p = nums[0]
+        max_product = max_p
         for num in nums[1:]:
             temp = min_p
-            min_p = min (num,num*min_p,num*max_p)
+            min_p = min(num,num*min_p,num*max_p)
             max_p = max(num,num*temp,num*max_p)
-            maximum_product = max(maximum_product,max_p)
-        return maximum_product
-        
+            max_product = max(max_p,max_product)
+        return max_product

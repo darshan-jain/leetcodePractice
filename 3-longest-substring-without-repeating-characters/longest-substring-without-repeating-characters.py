bcfc9d585ddbd84b0826c28184f3a35a53437afe
Darshan Jain <darshandhokajain@gmail.com>
2024-11-17 14:35:03 -0800

    Time: 22 ms (53.34%) | Memory: 11.9 MB (73.91%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,15 +5,13 @@ class Solution(object):
         :rtype: int
         """
         start = 0 
-        end = 0 
+        end =0 
         d = {}
         max_len = 0 
-
         while end < len(s):
-            if s[end] in d and d[s[end]] >= start:
+            if s[end] in d and d[s[end]] >= start : 
                 start = d[s[end]] + 1
-            max_len = max(max_len, end-start+1)
+            max_len = max(max_len,end-start+1)
             d[s[end]] = end
             end+=1
         return max_len
-        

c276e278839db9d438ea74c4e6972ff693e09ffc
Darshan Jain <darshandhokajain@gmail.com>
2024-12-03 10:51:06 -0800

    Time: 12 ms (31.00%) | Memory: 15.4 MB (9.99%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        prereqs = defaultdict(list)
+        for c,p in prerequisites:
+            prereqs[c].append(p)
+
+        def cycle(course,seen):
+            if course in seen:
+                return True 
+            seen.add(course)
+            for p in prereqs[course]:
+                if cycle(p,seen):
+                    return True
+            prereqs[course] = []
+            seen.remove(course)
+            return False
+
+        seen = set()
+
+        for course in range(numCourses):
+            if cycle(course,seen):
+                return False
+        return True
+        

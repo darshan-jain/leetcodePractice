d6b417d6e51966f61e7f332a2086d9e4ec9f26b9
Darshan Jain <darshandhokajain@gmail.com>
2024-12-16 15:33:36 -0800

    Time: 271 ms (35.55%) | Memory: 22.4 MB (93.98%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
-        max_edge =0 
+        max_edge = 0 
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
-                if row ==0 or col ==0 :
-                    max_edge = max(max_edge, int(matrix[row][col]))
-                elif matrix[row][col] == '1':
-                    matrix[row][col] = min(int(matrix[row-1][col]),int(matrix[row-1][col-1]),int(matrix[row][col-1])) +1
-                    max_edge = max(max_edge,matrix[row][col])        
+                if row ==0 or col==0:
+                    max_edge = max(max_edge,int(matrix[row][col]))
+                elif matrix[row][col]=='1':
+                    matrix[row][col] = min(int(matrix[row-1][col]), int(matrix[row-1][col-1]), int(matrix[row][col-1])) +1
+                    max_edge = max(max_edge,matrix[row][col])
         return max_edge**2
+
+        

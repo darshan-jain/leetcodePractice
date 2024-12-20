dfd81849f2668175511fa0121c343b0757ece049
Darshan Jain <darshandhokajain@gmail.com>
2024-12-20 15:10:31 -0500

    Time: 11 ms (21.94%) | Memory: 12.3 MB (16.06%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -6,11 +6,11 @@ class Solution(object):
         :rtype: bool
         """
         n = len(s) + 1
-        dp = [False]*n
+        dp = [False] * n
         dp[0] = True
         for i in range(n):
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
-                    break
         return dp[-1]
+        

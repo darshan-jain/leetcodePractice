0200ef39e738e7a688dc6fabaedcdc44e704bda6
Darshan Jain <darshandhokajain@gmail.com>
2024-12-21 12:52:57 -0500

    Time: 6 ms (95.26%) | Memory: 12.5 MB (9.23%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -5,20 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        
         candidates.sort()
         result = []
-
         def comb(curr,start,target):
-            if target==0:
+            if target ==0 :
                 result.append(curr)
                 return 
-            for i in range(start,len(candidates)):
+            for i in range(start, len(candidates)):
                 if candidates[i]>target:
                     break
-                comb(curr+[candidates[i]], i, target-candidates[i])
-            return
-
-
+                comb(curr+[candidates[i]], i, target- candidates[i])
 
         comb([],0,target)
         return result

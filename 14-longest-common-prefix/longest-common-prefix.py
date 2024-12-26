545e73a7e403f7fe138a830ca11b3b7fbfd80bdd
Darshan Jain <darshandhokajain@gmail.com>
2024-12-26 04:41:02 -0500

    Time: 3 ms (29.11%) | Memory: 17.8 MB (8.85%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,6 +3,6 @@ class Solution:
         prefix = strs[0]
         for i in range(len(prefix)):
             for word in strs:
-                if i == len(word) or prefix[i]  != word[i]:
+                if i == len(word) or prefix[i]!= word[i]:
                     return prefix[:i]
         return prefix

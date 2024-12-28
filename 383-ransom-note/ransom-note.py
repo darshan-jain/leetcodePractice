6f689d516840f53ebf5d2aee30eb1bf2da4217d5
Darshan Jain <darshandhokajain@gmail.com>
2024-12-28 12:47:44 -0500

    Time: 36 ms (8.65%) | Memory: 17.9 MB (12.10%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,22 +1,33 @@
 from collections import defaultdict
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dr = defaultdict(list)
-        dm = defaultdict(list)
-        letters = list(ransomNote)
-        for char in letters:
-            dr[char] = 0 
-        for char in letters:
-            dr[char] +=1
+        # dr = defaultdict(list)
+        # dm = defaultdict(list)
+        # letters = list(ransomNote)
+        # for char in letters:
+        #     dr[char] = 0 
+        # for char in letters:
+        #     dr[char] +=1
         
-        lettersmaga = list(magazine)
-        for char in lettersmaga:
-            dm[char] =0
-        for char in lettersmaga:
-            dm[char] +=1
+        # lettersmaga = list(magazine)
+        # for char in lettersmaga:
+        #     dm[char] =0
+        # for char in lettersmaga:
+        #     dm[char] +=1
         
-        for key,value in dr.items():
-            if key not in dm or value>dm[key]:
+        # for key,value in dr.items():
+        #     if key not in dm or value>dm[key]:
+        #         return False
+        # return True
+
+        d = defaultdict(list)
+        for char in magazine:
+            d[char] =0
+        for char in magazine:
+            d[char] += 1
+        for char in ransomNote:
+            if char not in d or d[char] <= 0 :
                 return False
+            d[char] -=1
         return True
         

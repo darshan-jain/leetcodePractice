41011244306acf01d32fff3585e22edff08d98aa
Darshan Jain <darshandhokajain@gmail.com>
2025-01-11 12:00:26 -0500

    Time: 19 ms (74.08%) | Memory: 18.3 MB (12.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,32 +1,15 @@
-class Solution(object):
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
         lst = [nums[0]]
         max_len = 1
         for num in nums[1:]:
             if num > lst[-1]:
                 lst.append(num)
-                max_len+=1
+                max_len +=1
             else:
-                # ind =0 
-                # while ind < len(lst) and lst[ind] < num:
-                #     ind+=1
-                # lst[ind] = num
-                left, right = 0,len(lst)-1
-                while left < right:
-                    mid = (left+right)//2
-                    if lst[mid] < num : 
-                        left = mid +1
-                    else:
-                        right = mid
-                lst[left] = num
-
-
+                ind =0 
+                while ind < len(lst) and lst[ind] < num:
+                    ind+=1
+                lst[ind] = num
         return max_len
-
-        #can be optimised for else part using binary search
-
         

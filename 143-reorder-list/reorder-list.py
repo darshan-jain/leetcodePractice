3ed9a75c3f40a6a1d139950c36a024d2af4e2d3a
Darshan Jain <darshandhokajain@gmail.com>
2025-01-11 11:04:08 -0800

    Time: 0 ms (100.00%) | Memory: 23.2 MB (36.12%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,17 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reorderList(self, head):
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
         """
-        :type head: Optional[ListNode]
-        :rtype: None Do not return anything, modify head in-place instead.
+        Do not return anything, modify head in-place instead.
         """
-        if head.next is None or head.next.next is None:
+        if not head.next or not head.next.next:
             return 
         
+        #split
         mid = end = head
         while end.next and end.next.next:
             end = end.next.next
@@ -19,24 +19,22 @@ class Solution(object):
         p2 = mid.next
         mid.next = None
 
+
         #reverse
         prev = None
         while p2 and p2.next:
             p2next = p2.next
             p2.next = prev
             prev = p2
-            p2= p2next
+            p2 = p2next
         p2.next = prev
 
+        #merge
         p1 = head
         while p1 and p2:
             p1next = p1.next
             p2next = p2.next
             p1.next = p2
             p2.next = p1next
-            p1 = p1next
+            p1= p1next
             p2 = p2next
-            
-
-        
-        

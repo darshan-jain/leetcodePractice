e41673fa4045e0b075ae58c5aa39b0f1e316a1d6
Darshan Jain <darshandhokajain@gmail.com>
2025-01-15 09:09:32 -0800

    Time: 3 ms (80.21%) | Memory: 18.4 MB (10.20%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
@@ -1,17 +1,24 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
 
-        N = len(people)
-        output = [None] * N
-        people.sort()
+        # N = len(people)
+        # output = [None] * N
+        # people.sort()
 
-        for height, q in people:
-            i,j = 0,-1
-            while i < N:
-                if not output[i] or output[i][0] == height:
-                    j+=1
-                if j==q:
-                    break
-                i+=1
-            output[i] = [height,q]
-        return output
+        # for height, q in people:
+        #     i,j = 0,-1
+        #     while i < N:
+        #         if not output[i] or output[i][0] == height:
+        #             j+=1
+        #         if j==q:
+        #             break
+        #         i+=1
+        #     output[i] = [height,q]
+        # return output
+
+
+        people = sorted(people, key = lambda x : (-x[0],x[1]))
+        ans = []
+        for p in people:
+            ans.insert(p[1],p)
+        return ans

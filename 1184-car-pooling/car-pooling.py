cb28399ba4557355f65fa6cda73cf0a751604162
Darshan Jain <darshandhokajain@gmail.com>
2025-01-18 14:06:33 -0800

    Time: 3 ms (67.78%) | Memory: 18 MB (65.23%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,18 +1,35 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        #trips - [numPass, start,end]
-        trips.sort(key = lambda t:t[1])
-        minHeap = []  # [end, numPass]
-        curPass = 0 
+
+        #O(n)
+
+        arr = [0] * 1001
         for t in trips:
-            numPass, start,end = t
-            while minHeap and minHeap[0][0] <= start:
-                curPass-=minHeap[0][1]
-                heapq.heappop(minHeap)
-
-
-            curPass+=numPass
+            numPass,start,end = t
+            arr[start]+=numPass
+            arr[end]-=numPass
+        curPass =0 
+        for i in range(len(arr)):
+            curPass+=arr[i]
             if curPass > capacity:
                 return False
-            heapq.heappush(minHeap, [end,numPass])
         return True
+
+
+        #O(nlogn Approach)
+        #trips - [numPass, start,end]
+        # trips.sort(key = lambda t:t[1])
+        # minHeap = []  # [end, numPass]
+        # curPass = 0 
+        # for t in trips:
+        #     numPass, start,end = t
+        #     while minHeap and minHeap[0][0] <= start:
+        #         curPass-=minHeap[0][1]
+        #         heapq.heappop(minHeap)
+
+
+        #     curPass+=numPass
+        #     if curPass > capacity:
+        #         return False
+        #     heapq.heappush(minHeap, [end,numPass])
+        # return True

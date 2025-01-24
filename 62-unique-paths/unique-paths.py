b2a8d85fc9d95f2bff743abb9f3b3ff3593d1336
Darshan Jain <darshandhokajain@gmail.com>
2025-01-23 23:14:56 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (59.57%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,21 +1,10 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        if m==n==1:
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == n == 1:
             return 1
-        dp = []
-        for i in range(n):
-            row = []
-            for j in range(m):
-                row.append(1)
-            dp.append(row)
+        dp =  [[1]*m]*n
         for row in range(1,len(dp)):
-            for col in range(1,len(dp[row])):
-                dp[row][col] = dp[row-1][col]+dp[row][col-1]
+            for col in range(1,len(dp[0])):
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
         return dp[-1][-1]
-
         

0190b8e0c9b2de2f6cb0bc48610de9636b8b8ab8
Darshan Jain <darshandhokajain@gmail.com>
2025-01-28 10:23:07 -0800

    Time: 4 ms (63.33%) | Memory: 18.1 MB (14.44%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.py
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.py
@@ -1,18 +1,39 @@
 class Solution:
     def countMatchingSubarrays(self, nums: List[int], pattern: List[int]) -> int:
-        ans = 0 
+        # ans = 0 
+        # n = len(nums)
+        # m = len(pattern)
+
+        # for i in range(0,n-m):
+        #     flag = 0 
+        #     for k in range(0,m):
+        #         if (pattern[k]==1 and nums[i+k+1] > nums[i+k]) or (pattern[k]==0 and nums[i+k+1] == nums[i+k]) or (pattern[k]==-1 and nums[i+k+1] < nums[i+k]):
+        #             continue
+        #         else:
+        #             flag = 1
+        #             break
+        #     if flag == 0 :
+        #         ans+=1
+        # return ans
+
+
         n = len(nums)
         m = len(pattern)
-
+        ans = 0 
+        v = []
+        for i in range(n-1):
+            if nums[i] < nums[i+1]:
+                v.append(1)
+            elif nums[i] == nums[i+1]:
+                v.append(0)
+            else:
+                v.append(-1)
+        
         for i in range(0,n-m):
-            flag = 0 
-            for k in range(0,m):
-                if (pattern[k]==1 and nums[i+k+1] > nums[i+k]) or (pattern[k]==0 and nums[i+k+1] == nums[i+k]) or (pattern[k]==-1 and nums[i+k+1] < nums[i+k]):
-                    continue
-                else:
-                    flag = 1
-                    break
-            if flag == 0 :
+            if v[i:i+m] == pattern:
                 ans+=1
         return ans
+
+
+
         

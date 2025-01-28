59d9fea5ede677b8242d1626b5055c2c134013ae
Darshan Jain <darshandhokajain@gmail.com>
2025-01-28 14:49:16 -0800

    Time: 15 ms (43.50%) | Memory: 18.5 MB (8.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,10 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        #return Counter(s) == Counter(t)
-
-        s_counts = Counter(s)
-        for char in t:
-            if char not in s_counts:
-                return False
-            s_counts[char]-=1
-            if s_counts[char] ==0:
-                del s_counts[char]
-        return len(s_counts)==0
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        if s==t:
+            return True
+        return False
+        

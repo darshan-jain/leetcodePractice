be8d4f45a5270d4e47764fdec78ba71a949bad79
Darshan Jain <darshandhokajain@gmail.com>
2025-01-28 21:37:32 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (30.88%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,24 +1,22 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        beg,end = 0, len(nums)-1
-        while beg < end:
-            mid = (beg+end)//2
-            if nums[mid] == target:
-                return mid
-            if nums[beg] <=nums[mid]:
-                if nums[beg]<=target<=nums[mid]:
-                    end = mid-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0 
+        r = len(nums)-1
+
+        while l <= r : 
+            m = (l+r)//2
+            if nums[m] == target:
+                return m 
+            if nums[l] <= nums[m]:
+                if nums[l] <= target <= nums[m]:
+                    r = m - 1
                 else:
-                    beg = mid+1
+                    l = m+1
             else:
-                if nums[mid] <= target <= nums[end]:
-                    beg = mid+1
+                if nums[m] <= target <= nums[r]:
+                    l = m+1
                 else:
-                    end = mid-1
-        return -1 if nums[end]!=target else end
+                    r = m-1
+        
+        return -1
         

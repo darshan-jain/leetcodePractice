064350aefde30bafdb0b7ea6acffbd1c335ed53b
Darshan Jain <darshandhokajain@gmail.com>
2025-02-11 23:08:49 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.17%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,16 +1,17 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        if len(nums)==1 and nums[0] ==0:
-            return 0
-        time =0
-        while sum(nums)!= 0:
-            copy = [ele for ele in nums if ele!=0]
-            x = min(copy)            
-            for i in range(len(nums)):
-                if nums[i]!=0:
-                    nums[i]-=x
-            time+=1
-            print(nums)
-        return time
+        # if len(nums)==1 and nums[0] ==0:
+        #     return 0
+        # time =0
+        # while sum(nums)!= 0:
+        #     copy = [ele for ele in nums if ele!=0]
+        #     x = min(copy)            
+        #     for i in range(len(nums)):
+        #         if nums[i]!=0:
+        #             nums[i]-=x
+        #     time+=1
+        #     print(nums)
+        # return time
 
-        
+        unique_set = {number for number in nums if number!=0}
+        return len(unique_set)

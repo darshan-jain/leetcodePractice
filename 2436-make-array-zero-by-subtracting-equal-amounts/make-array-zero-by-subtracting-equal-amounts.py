1d3c2be42c22781a5505467887e590d3e41295f4
Darshan Jain <darshandhokajain@gmail.com>
2025-02-11 23:06:44 -0800

    Time: 16 ms (6.08%) | Memory: 17.5 MB (94.69%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        if len(nums)==1 and nums[0] ==0:
+            return 0
+        time =0
+        while sum(nums)!= 0:
+            copy = [ele for ele in nums if ele!=0]
+            x = min(copy)            
+            for i in range(len(nums)):
+                if nums[i]!=0:
+                    nums[i]-=x
+            time+=1
+            print(nums)
+        return time
+
+        

1fc0d4dcec15f9123a183ef6e08b5c516e594048
Darshan Jain <darshandhokajain@gmail.com>
2025-02-13 13:48:33 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (94.14%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         def dfs(root1,root2):
-            if root1 == root2:
+            if root1 is None and root2 is None:
                 return True
             if root1 is None or root2 is None or root1.val!=root2.val:
                 return False

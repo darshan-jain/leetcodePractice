377b7daf5207af4f687c823cca67eb021c4b0867
Darshan Jain <darshandhokajain@gmail.com>
2025-02-18 12:18:11 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.37%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -8,3 +8,7 @@ class Solution:
                 dp[row][col] = dp[row-1][col] + dp[row][col-1]
         return dp[-1][-1]
         
+        #Recursive Soln  - TLE 
+        # if m==1 or n==1:
+        #     return 1
+        # return self.uniquePaths(m-1,n) + self.uniquePaths(m,n-1)

ce42ff6ff342660b951b7bcc24195a329750593e
Darshan Jain <darshandhokajain@gmail.com>
2025-02-21 15:10:48 -0800

    Time: 15 ms (59.87%) | Memory: 20.1 MB (62.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def minPathSum(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if row ==0 and col !=0:
-                    grid[row][col] += grid[row][col-1]
-                elif col==0 and row !=0 : 
-                    grid[row][col] += grid[row-1][col]
-                elif col!=0 and row!=0:
-                    grid[row][col] += min(grid[row-1][col],grid[row][col-1])
-        return grid[row][col]
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for i in range(1,n):
+            dp[0][i] = grid[0][i] + dp[0][i-1]
+        for i in range(1,m):
+            dp[i][0]= grid[i][0]  + dp[i-1][0]
+
+        for row in range(1,m):
+            for col in range(1,n):
+                dp[row][col] = min(dp[row-1][col]+grid[row][col],dp[row][col-1]+ grid[row][col])
+
+        return dp[m-1][n-1]
         

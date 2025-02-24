2d5871a6b3342e0ff41e2cfea34f3fccfbc23e82
Darshan Jain <darshandhokajain@gmail.com>
2025-02-24 12:27:55 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.39%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,15 +1,25 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == n == 1:
-            return 1
-        dp =  [[1]*m]*n
+        # if m == n == 1:
+        #     return 1
+        # dp =  [[1]*m]*n
+        # for row in range(1,len(dp)):
+        #     for col in range(1,len(dp[0])):
+        #         dp[row][col] = dp[row-1][col] + dp[row][col-1]
+        # return dp[-1][-1]
+
+     
+        #Recursive Soln
+        # if m==1 or n==1:
+        #     return 1
+        # if r>0 and c>0:
+        #     return self.uniquePaths(m-1,n) + self.uniquePaths(m,n-1)
+
+        if m == n ==1:
+            return 1 
+        dp = [[1]*m]*n
         for row in range(1,len(dp)):
             for col in range(1,len(dp[0])):
                 dp[row][col] = dp[row-1][col] + dp[row][col-1]
-        return dp[-1][-1]
         
-        #Recursive Soln  - TLE 
-        if m==1 or n==1:
-            return 1
-        if r>0 and c>0:
-            return self.uniquePaths(m-1,n) + self.uniquePaths(m,n-1)
+        return dp[-1][-1]

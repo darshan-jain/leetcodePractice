bf382f066807f57bc77c26799b7297785cf60447
Darshan Jain <darshandhokajain@gmail.com>
2025-02-23 20:14:07 -0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (89.20%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -10,16 +10,34 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        dummy = ListNode(0,head)
-        p1 = head
-        p2 = dummy
-        counts = 0
-        while p1 != None:
-            p1 = p1.next
-            counts+=1
-            if counts > n:
-                p2 = p2.next
-        p2.next = p2.next.next
-        return dummy.next
+        # dummy = ListNode(0,head)
+        # p1 = head
+        # p2 = dummy
+        # counts = 0
+        # while p1 != None:
+        #     p1 = p1.next
+        #     counts+=1
+        #     if counts > n:
+        #         p2 = p2.next
+        # p2.next = p2.next.next
+        # return dummy.next
+
+        root = ListNode(0)
+        root.next = head 
+        first = root
+        second = root
+        for i in range(n+1):
+            first = first.next 
+        
+        while first is not None:
+            first = first.next 
+            second = second.next
+        
+        second.next = second.next.next 
+
+        return root.next
+        
+
+
         
         

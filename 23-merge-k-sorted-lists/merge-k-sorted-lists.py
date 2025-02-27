f7504bd574fb200e4f8331fe4caa05d41e030122
Darshan Jain <darshandhokajain@gmail.com>
2025-02-27 10:14:49 -0800

    Time: 8 ms (79.47%) | Memory: 20.4 MB (35.49%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,27 +1,23 @@
-import heapq
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
-        """
-        min_heap = []
-        for i, node in enumerate(lists):
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        minHeap = []
+        for i,node in enumerate(lists):
             if node:
-                heapq.heappush(min_heap,(node.val,i,node))
+                heapq.heappush(minHeap, (node.val,i,node))
         dummy = ListNode(0)
         curr = dummy
-        while min_heap:
-            val,i,node = heapq.heappop(min_heap)
+        while minHeap:
+            val,i,node = heapq.heappop(minHeap)
             curr.next = node
             curr = curr.next
             if node.next:
-                heapq.heappush(min_heap,(node.next.val,i,node.next))
+                heapq.heappush(minHeap,(node.next.val,i,node.next))
+        
         return dummy.next
-
         

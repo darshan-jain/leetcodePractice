71adfa9e902eb2f62769eff4fee6a50925c894a5
Darshan Jain <darshandhokajain@gmail.com>
2025-03-01 09:36:15 -0800

    Time: 11 ms (53.78%) | Memory: 31.5 MB (82.88%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,25 +1,10 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        # Hashmap Approach
-        # seen = {}
-        # for i in range(len(nums)):
-        #     seen[nums[i]] = 0
-        # for num in nums:
-        #     seen[num] += 1
-        #     if seen[num] >= 2:
-        #         return True
-            
-
-        # return False
-
-        # set data structure approach 
-        setnums = set()
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
         for num in nums:
-            if num in setnums:
+            if num in seen:
                 return True
-            setnums.add(num)
+            seen.add(num)
+        
         return False
+        

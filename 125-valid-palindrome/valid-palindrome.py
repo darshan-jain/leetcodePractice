7eff37ba204dcf3b6cd14ac1130b4f473c1862fa
Darshan Jain <darshandhokajain@gmail.com>
2025-03-01 11:29:44 -0800

    Time: 8 ms (53.77%) | Memory: 18.2 MB (57.67%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        left, right = 0 ,len(s)-1
-        while left<right:
-            while left < right and not s[left].isalnum():
-                left+=1
-            while left < right and not s[right].isalnum():
-                right-=1
-            if s[left].lower() != s[right].lower():
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0 
+        r = len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l+=1
+            while l < r and not s[r].isalnum():
+                r-=1
+            if s[l].lower() != s[r].lower():
                 return False
-            left+=1
-            right-=1
+            
+            l+=1
+            r-=1
         return True
+        

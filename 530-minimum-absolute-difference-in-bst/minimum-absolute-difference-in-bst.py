7d8d4f18d3e30f804a4e81d9911861e11a671c7c
Darshan Jain <darshandhokajain@gmail.com>
2025-02-28 16:50:18 -0800

    Time: 4 ms (54.62%) | Memory: 19.6 MB (84.93%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -22,13 +22,13 @@ class Solution:
         self.min_diff = float("inf")
         self.prev_val = None
         def dfs(root):
-            if root is None:
-                return 
+            if not root:
+                return
             dfs(root.left)
             if self.prev_val is not None:
-                self.min_diff = min(self.min_diff, abs(self.prev_val - root.val))
+                self.min_diff = min(self.min_diff,abs(self.prev_val - root.val))
             self.prev_val = root.val
             dfs(root.right)
         dfs(root)
         return self.min_diff
-        
+                

65ec50572ca1da9c66f796e266786c3e8d60a77a
Darshan Jain <darshandhokajain@gmail.com>
2025-03-02 16:51:27 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.65%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,15 +2,14 @@ class Solution:
     def helper(self,n,memo={}):
         if n in memo:
             return memo[n]
-        if n==0:
-            return 1 
-        if n <0:
+        if n == 0 :
+            return 1
+        if n < 0 :
             return 0 
-        left = self.helper(n-1)    
+        left = self.helper(n-1)
         right = self.helper(n-2)
         memo[n] = val = left+right
         return val
     def climbStairs(self, n: int) -> int:
-        ans = self.helper(n)
-        return ans
+        return self.helper(n)
         

a5d7264050c324ea30be5a0acff6eef97136fa4c
Darshan Jain <darshandhokajain@gmail.com>
2025-03-03 11:34:05 -0800

    Time: 7 ms (8.26%) | Memory: 19.6 MB (90.45%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,24 +1,18 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        i=0
+        i = 0 
         n = len(intervals)
 
-        while (i<n and intervals[i][1]<newInterval[0]):
+        while i <n and intervals[i][1]<newInterval[0]:
             res.append(intervals[i])
-            i = i+1
-        while i< n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0],intervals[i][0])
-            newInterval[1] = max(newInterval[1],intervals[i][1])
-            i=i+1
+            i+=1
+        while i < n and intervals[i][0]<=newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])        
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i+=1
         res.append(newInterval)
-        while i<n:
+        while i < n :
             res.append(intervals[i])
-            i=i+1
+            i+=1
         return res
-        

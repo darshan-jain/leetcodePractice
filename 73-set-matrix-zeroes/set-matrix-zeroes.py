ef3fbef74cccc9f2d665fb23e4db7cec01d56517
Darshan Jain <darshandhokajain@gmail.com>
2025-03-03 13:04:50 -0800

    Time: 0 ms (100.00%) | Memory: 18.3 MB (95.42%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,28 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowflag = colflag = False
-        rows = len(matrix)
-        cols = len(matrix[0])
-
+        rowFlag = False
+        colFlag = False
+        rows, cols = len(matrix) , len(matrix[0])
         for row in range(rows):
             for col in range(cols):
-                if matrix[row][col]==0:
-                    if row ==0:
-                        rowflag = True
-                    if col ==0:
-                        colflag = True
-                    elif row!=0 and col!=0:
-                        matrix[row][0] = 0
-                        matrix[0][col] = 0 
+                if matrix[row][col] == 0 :
+                    if row==0:
+                        rowFlag = True
+                    if col==0:
+                        colFlag = True
+                    else:
+                        if row!=0 and col!=0:
+                            matrix[row][0] =0  
+                            matrix[0][col] = 0 
         
         for row in range(1,rows):
             for col in range(1,cols):
-                if matrix[row][0] ==0 or matrix[0][col] ==0:
-                    matrix[row][col] = 0 
-        if rowflag:
+                if matrix[0][col]==0 or matrix[row][0]==0:
+                    matrix[row][col]=0
+        
+        if rowFlag:
             matrix[0] = [0]*cols
-        if colflag:
-            for row in range(rows):
-                matrix[row][0] = 0
+        
+        if colFlag:
+            for i in range(rows):
+                matrix[i][0] = 0
         

4760050b91b819e920fb372c69cf65339498d8c4
Darshan Manoj <darshandhokajain@gmail.com>
2025-04-05 12:18:09 -0700

    Time: 3 ms (81.98%) | Memory: 17.9 MB (27.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,9 +1,9 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0 
+        fresh =0 
         rotten = []
-        rows,cols= len(grid) , len(grid[0])
         time = 0 
+        rows,cols = len(grid), len(grid[0])
 
         for row in range(rows):
             for col in range(cols):
@@ -11,10 +11,10 @@ class Solution:
                     fresh+=1
                 if grid[row][col]==2:
                     rotten.append((row,col))
-        
+
         while rotten and fresh > 0 :
             time+=1
-            curr= []
+            curr = []
             for r,c in rotten:
                 lst = [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
                 for row,col in lst:
@@ -30,3 +30,4 @@ class Solution:
             return time
         else:
             return -1
+        

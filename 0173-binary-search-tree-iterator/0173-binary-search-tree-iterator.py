760ca634ea2bd611f8671e0fe9d4be674af15709
Darshan Manoj <darshandhokajain@gmail.com>
2025-04-18 15:22:00 -0700

    0173-BINARY-SEARCH-TREE-ITERATOR Stats: Time: 11 ms (34.45%), Space: 25.2 MB (18.95%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -10,20 +10,23 @@ class BSTIterator:
         self.stack = []
         while root:
             self.stack.append(root)
-            root = root.left
+            root= root.left
+        
 
     def next(self) -> int:
         res = self.stack.pop()
         cur = res.right
         while cur:
             self.stack.append(cur)
-            cur = cur.left
+            cur = cur.left 
         return res.val
-
+        
 
     def hasNext(self) -> bool:
         return self.stack!=[]
         
+
+
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
 # param_1 = obj.next()

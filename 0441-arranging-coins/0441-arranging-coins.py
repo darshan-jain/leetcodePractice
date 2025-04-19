a3e0ff266ee3bd1295178977e141f1de1bbf3ecf
Darshan Manoj <darshandhokajain@gmail.com>
2025-04-19 15:40:18 -0700

    0441-ARRANGING-COINS Stats: Time: 1 ms (70.98%), Space: 17.9 MB (32.38%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,15 +1,26 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n==1 or n==2:
-            return 1
-        remCoins = n
-        for i in range(1,n):
-            remCoins = remCoins - i
-            if remCoins==0:
-                return i
-            if remCoins <0:
-                return i-1
-        
+        # if n==1 or n==2:
+        #     return 1
+        # remCoins = n
+        # for i in range(1,n):
+        #     remCoins = remCoins - i
+        #     if remCoins==0:
+        #         return i
+        #     if remCoins <0:
+        #         return i-1
+
+        l,r = 1, n 
+        res = 0 
+        while l <=r:
+            mid = (l+r)//2
+            coins = (mid/2) * (mid+1)
+            if coins > n:
+                r = mid-1
+            else:
+                l = mid+1
+                res = max(res,mid)
+        return res
         
 
             

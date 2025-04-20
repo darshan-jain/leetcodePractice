67c780e8d5773e8dbc390e55094a993fb8b01ea2
Darshan Manoj <darshandhokajain@gmail.com>
2025-04-20 11:40:41 -0700

    1023-TIME-BASED-KEY-VALUE-STORE Stats: Time: 124 ms (56.17%), Space: 75.7 MB (5.22%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -12,19 +12,17 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
-        values = self.store.get(key, [])
-        l,r = 0, len(values)-1
+        values = self.store.get(key,[])
+        l=0
+        r = len(values)-1
         while l<=r:
-            m = (l+r) // 2
+            m = (l+r)//2
             if values[m][1] <=timestamp:
                 res = values[m][0]
-                l= m+1
+                l = m+1
             else:
                 r = m-1
         return res
-            
-
-
         
 
 

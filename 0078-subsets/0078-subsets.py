33d8074e6d502debe8e28647c7ff3e903af142e5
Darshan Manoj <darshandhokajain@gmail.com>
2025-04-23 22:58:55 -0700

    0078-SUBSETS Stats: Time: 0 ms (100%), Space: 18 MB (21.5%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,14 @@
 class Solution:
     def __init__(self):
         self.ans = []
-    def helper(self, p, up):
+    def helper(self,p,up):
         if len(up)==0:
             self.ans.append(p)
             return 
-        ch = up[0]
+        ch= up[0]
         self.helper(p+[ch],up[1:])
         self.helper(p,up[1:])
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.helper([],nums)
         return self.ans

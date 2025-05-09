c4743b8c815ffb7c3e58b96ce0dfad94035365df
Darshan Manoj <darshandhokajain@gmail.com>
2025-05-09 15:06:01 -0700

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 19 ms (77.84%), Space: 12.5 MB (96.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: int
         """
         ans = 0 
-        j = -1
+        j=-1
         lastseen = {}
-        for i,c in enumerate(s):
-            j = max(j , lastseen.get(c,-1))
-            ans = max(ans, i - j)
-            lastseen[c] = i
+        for i, c in enumerate(s):
+            j = max(j,lastseen.get(c,-1))
+            ans = max(ans, i-j)
+            lastseen[c] = i 
         return ans
-
-
-        

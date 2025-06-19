b397bfbf2ffdbaf3c29848f45bbd48c90a2949bb
Darshan Manoj <darshandhokajain@gmail.com>
2025-06-19 14:25:56 -0700

    0056-MERGE-INTERVALS Stats: Time: 2 ms (93.26%), Space: 24 MB (33.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-        
-        if (a.empty() || b.empty()) {
-            return false; // Or a more robust comparison for empty vectors
-        }
-        return a[0] < b[0]; // Sorts in ascending order based on the first element
-    });
-    ans.push_back(intervals[0]);
-    vector<int> lastval ;
-    for(int i=0;i<intervals.size();i++){
-        lastval= ans.back();
-        if(lastval[1] >= intervals[i][0]){
-            //merge
-            if(lastval[1] < intervals[i][1]){
-                lastval[1] = intervals[i][1];
-                ans.back() = lastval;
+        sort(intervals.begin(), intervals.end(), [](vector<int>&a,vector<int>&b ) {
+            if(a.empty()||b.empty())
+            return false;
+            return a[0]<b[0];
+        });
+
+        ans.push_back(intervals[0]);
+        vector<int> lastelem;
+        for(int i=1;i<intervals.size();i++){
+            lastelem = ans.back();
+            if(lastelem[1]>= intervals[i][0]){
+                //merge
+                if(lastelem[1]<intervals[i][1])
+                {
+                    lastelem[1]=intervals[i][1];
+                    ans.back() = lastelem;
+                }
             }
+            else{
+                ans.push_back(intervals[i]);
+            }
+
         }
-        else{
-            ans.push_back(intervals[i]);
-        }
+        return ans;
 
-    }
-    
-    return ans;
-
-
+        
     }
 };

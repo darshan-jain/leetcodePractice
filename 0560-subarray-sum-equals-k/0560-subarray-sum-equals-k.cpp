e5ef79f94df4208c8d3ebd14acd48bb81825e4dd
Darshan Manoj <darshandhokajain@gmail.com>
2025-07-02 12:03:51 -0700

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 34 ms (91.99%), Space: 45.5 MB (45.37%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> mpp;
-        int res=0;
-        int diff=0;
+        int res = 0 ;
+        int diff = 0 ;
         int curSum = 0 ;
-
         mpp[0]=1;
-        for(int i=0;i<nums.size();i++)
+
+        for(auto n: nums)
         {
-            curSum+=nums[i];
-            diff = curSum-k;
-
-            if(mpp.find(diff)!=mpp.end())
-            {res+= mpp[diff];
-            cout<<"Found"<<diff<<endl;
+            curSum+=n;
+            
+            diff = curSum - k;
+            if(mpp.find(diff)!=mpp.end()){
+                res+=mpp[diff];
             }
-
             mpp[curSum]++;
         }
-        for(auto it:mpp)
-        cout<< it.first<<" "<<it.second<<endl;
-
         return res;
         
     }

ae5614315c4e0f308982da07ac8b08ef51a0e55a
Darshan Manoj <darshandhokajain@gmail.com>
2025-08-11 13:07:27 -0700

    0160-INTERSECTION-OF-TWO-LINKED-LISTS Stats: Time: 31 ms (90.01%), Space: 18.5 MB (52.93%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,79 +9,21 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-
-        // int sizeA =0 ;
-        // int sizeB = 0 ;
-        // ListNode *currA;
-        // currA = headA;
-        // while (currA){
-        //     sizeA++;
-        //     currA=currA->next;
-        // }
-
-        // ListNode *currB;
-        // currB = headB;
-        // while (currB){
-        //     sizeB++;
-        //     currB=currB->next;
-        // }
-
-        // //make sure A is big - so exchange A & B if sizeA < sizeB
-        // if(sizeA<sizeB)
-        // {
-        //     ListNode *temp = headA;
-        //     headA = headB;
-        //     headB = temp;
-        // }
-
-        // //A is always big 
-
-        // ListNode *startptrA;
-        // startptrA = headA;
-        
-        // while(startptrA){
-        //     ListNode *valA = startptrA;
-        //     ListNode *valB = headB;
-        //     while(valA && valB)
-        //     {
-        //         if(valA == valB)
-        //         {
-        //             return valA;
-        //         }
-        //         else{
-        //             valA = valA->next;
-        //             valB = valB->next;
-        //         }
-        //     }
-        //     startptrA = startptrA->next;
-        // }
-
-        // return NULL;
-
-        ListNode *l1,*l2;
+        ListNode *l1, *l2;
         l1 = headA;
         l2 = headB;
         while(l1!=l2)
         {
             if(l1)
-            {
-                l1 = l1->next;
-            }
-            else{
-                l1 = headB;
-            }
-
+            l1 = l1->next;
+            else 
+            l1 = headB;
             if(l2)
-            {
-                l2 = l2->next;
-            }
-            else{
-                l2 = headA;
-            }
+            l2 = l2->next;
+            else 
+            l2 = headA;
         }
-
         return l1;
-
         
     }
 };

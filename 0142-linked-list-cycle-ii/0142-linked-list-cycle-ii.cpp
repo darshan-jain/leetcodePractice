59c814093387067ca4685cd553053c6e8a89150e
Darshan Manoj <darshandhokajain@gmail.com>
2025-08-11 13:14:03 -0700

    0142-LINKED-LIST-CYCLE-II Stats: Time: 3 ms (98.2%), Space: 11.4 MB (24.25%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,55 +9,32 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        int hasCycle = 0 ;
+        int hasCycle = 0;
         ListNode * slow = head;
-        ListNode * temp = head;
-        ListNode * fast = head;
+        ListNode *fast = head;
         while(fast && fast->next)
         {
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast)
             {
-                hasCycle = 1;
+                hasCycle=1;
                 break;
             }
+
         }
 
-        
-        // O(n^2) Approach
-        // if(hasCycle)
-        // {
-        //     while(temp)
-        //     {
-        //         do
-        //         {
-        //             if(temp==slow)
-        //             {
-        //                 return temp;
-        //             }
-        //             slow = slow->next;
-        //         }while(slow!=fast);
-        //         //fast = slow;
-        //         temp = temp->next;
-        //     }
-        // }
-        // else{
-        //     return NULL;
-        // }
-
-        //O(n) Approach 
-        if(hasCycle)
+        if(hasCycle==0)
         {
-            ListNode * ptr = head;
-            while(ptr!=fast)
-            {
-                ptr= ptr->next;
-                fast =fast->next;
-            }
-            return ptr;
+            return NULL;
         }
-        return NULL;
+        ListNode *ptr = head;
+        while(ptr!=slow)
+        {
+            ptr = ptr->next;
+            slow = slow->next;
+        }
+        return ptr;
         
     }
 };

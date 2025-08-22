0b5cbbcdbd5ea38473425b5ef11f2e64dddb8b7a
Darshan Manoj <darshandhokajain@gmail.com>
2025-08-22 14:44:57 -0700

    0131-PALINDROME-PARTITIONING Stats: Time: 50 ms (55.4%), Space: 34.4 MB (81.47%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,24 +1,24 @@
 class Solution:
-    def isPali(self,s,l,r):
-        while l<r:
-            if s[l]!=s[r]:
-                return False
-            l,r = l+1,r-1
-        return True
     def partition(self, s: str) -> List[List[str]]:
+        def isPali(s,l,r):
+            while l<=r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
         res = []
         part = []
 
         def backtrack(i):
             if i>=len(s):
-                res.append(part.copy())
+                res.append(part[:])
                 return 
             for j in range(i,len(s)):
-                if self.isPali(s,i,j):
+                if isPali(s,i,j):
                     part.append(s[i:j+1])
                     backtrack(j+1)
                     part.pop()
-        
         backtrack(0)
         return res
         

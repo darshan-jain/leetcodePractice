12b3ebd267c5cf00d95a2dc56513929e59de91ce
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-16 00:13:29 -0700

    0801-IS-GRAPH-BIPARTITE Stats: Time: 1 ms (55.77%), Space: 18.5 MB (54.15%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,26 +1,20 @@
-from collections import deque
 class Solution:
-    def check(self,start,V,graph, color):
-        q = deque()
-        q.append(start)
-        color[start] = 0 
-        while q:
-            node = q.popleft()
-            for val in graph[node]:
-                if color[val]==-1:
-                    color[val] = 1-color[node]
-                    q.append(val)
-                elif color[val]==color[node]:
+    def dfs(self, node, col, graph, color):
+        color[node] = col
+        for val in graph[node]:
+            if color[val]==-1:
+                if self.dfs(val,not col, graph,color)==False:
                     return False
+            elif color[val]==color[node]:
+                return False
         return True
-            
 
     def isBipartite(self, graph: List[List[int]]) -> bool:
         V = len(graph)
         color = [-1 for _ in range(V)]
         for i in range(V):
             if color[i]==-1:
-                if self.check(i,V,graph, color)==False:
+                if self.dfs(i,0,graph, color)==False:
                     return False
         return True
         

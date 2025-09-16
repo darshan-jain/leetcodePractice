06a51b77b2c46bcfc175bb88ace6f217b9c8b2a3
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-16 08:40:08 -0700

    0207-COURSE-SCHEDULE Stats: Time: 5 ms (57.32%), Space: 19.3 MB (51.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,29 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
-        prereq = defaultdict(list)
-        for p,c in prerequisites:
-            prereq[c].append(p)
+        #build the graph - adj list 
+        graph = defaultdict(list)
+        for a,b in prerequisites:
+            graph[a].append(b)
         
+        #detect cycle 
         def cycle(course, seen):
             if course in seen:
                 return True
             seen.add(course)
-            for c in prereq[course]:
+            for c in graph[course]:
                 if cycle(c,seen):
                     return True
-            prereq[course] = []
             seen.remove(course)
+            graph[course] = []
             return False
-            
 
-        
-        seen = set()  # set to keep track of courses done so far
-        for course in range(numCourses):
-            if cycle(course, seen):
-                return False
+        seen = set()
+        for i in range(numCourses):
+            for course in graph[i]:
+                if cycle(course,seen):
+                    return False
         return True
+
+
         

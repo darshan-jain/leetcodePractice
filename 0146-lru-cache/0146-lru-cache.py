67d1e1a5fcc16f7349436ef71443bec2f8664700
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-23 12:50:08 -0700

    0146-LRU-CACHE Stats: Time: 153 ms (40.52%), Space: 78.1 MB (43.3%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,31 +1,31 @@
 class Node:
-    def __init__(self, key,val):
-        self.key = key 
+    def __init__(self,key,val):
+        self.key = key
         self.val = val
-        self.prev = self.next = None
+        self.prev = None 
+        self.next = None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity 
+        self.cap = capacity
         self.cache = {}
         self.left = Node(0,0)
         self.right = Node(0,0)
-        self.left.next  = self.right 
+        self.left.next = self.right 
         self.right.prev = self.left
-        
-    def insert(self, node):
-        prv , end = self.right.prev, self.right
+    
+    def insert(self,node):
+        prv, nxt = self.right.prev, self.right 
         prv.next = node 
-        node.next = end
-        end.prev = node 
-        node.prev = prv
+        node.prev = prv 
+        node.next = nxt 
+        nxt.prev = node
     
     def remove(self,node):
-        prv, nxt = node.prev, node.next
+        prv , nxt = node.prev, node.next 
         prv.next = nxt 
         nxt.prev = prv
-
-
+    
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -38,11 +38,11 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key,value)
+        self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
-            lru = self.left.next
+            lru  = self.left.next 
             self.remove(lru)
             del self.cache[lru.key]
         

24d4eb0dff96bda0d304cca93b243d2a6b34ef6a
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-23 13:47:52 -0700

    0025-REVERSE-NODES-IN-K-GROUP Stats: Time: 0 ms (100%), Space: 16.4 MB (70.46%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,11 +10,11 @@
  */
 class Solution {
 public:
-ListNode *getkth(ListNode *curr, int k)                                                                            
+ListNode * getkth(ListNode *curr, int k )
 {
     while(curr && k>0)
     {
-        curr= curr->next;
+        curr = curr->next;
         k--;
     }
     return curr;
@@ -25,25 +25,26 @@ ListNode *getkth(ListNode *curr, int k)
         while(1)
         {
             ListNode *kth = getkth(groupprev,k);
-            if(kth == NULL)
+            if(kth== NULL)
             {
                 break;
             }
             ListNode *groupnext = kth->next;
-            ListNode * prev = groupnext;
+
+            ListNode *prev = groupnext;
             ListNode *curr = groupprev->next;
             while(curr!=groupnext)
             {
-                ListNode * temp = curr->next;
+                ListNode *temp = curr->next;
                 curr->next = prev;
                 prev = curr;
                 curr = temp;
             }
-            ListNode * temp = groupprev->next;
+            ListNode *temp = groupprev->next;
             groupprev->next = kth;
             groupprev = temp;
-
         }
         return dummy->next;
+        
     }
 };

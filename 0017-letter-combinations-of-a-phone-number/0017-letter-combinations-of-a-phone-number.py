9cc57d5102b02c780b915675b352ace318773e3b
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-27 18:10:23 -0700

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 0 ms (100%), Space: 18 MB (23.3%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
-        digittochar = {
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
+        digittostr = {
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':"jkl",
+            '6':"mno",
+            '7':"pqrs",
+            '8':"tuv",
+            '9':"wxyz"
         }
+        def backtrack(index, curStr):
+            if index==len(digits):
+                res.append(curStr[:])
+                return 
+            for j in digittostr[digits[index]]:
+                backtrack(index+1, curStr + j)
 
-        def backtrack(index,curStr):
-            if index == len(digits):
-                res.append(curStr)
-                return
-            for c in digittochar[digits[index]]:
-                backtrack(index+1,curStr+c)
-        
+
         if digits:
             backtrack(0,"")
-        
         return res
         

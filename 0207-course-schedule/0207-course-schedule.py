4477a490dccaac26da770b8a9306fcf9e9fa87c1
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-28 23:55:04 -0700

    0207-COURSE-SCHEDULE Stats: Time: 11 ms (13.72%), Space: 19.4 MB (40.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,12 +1,13 @@
+
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
         for a,b in prerequisites:
             graph[a].append(b)
-
+        
         seen = set()
-        #returns True if cycle exists
+        #this fnc returns True if cycle exits, False otherwise
         def cycle(course,seen):
             if course in seen:
                 return True
@@ -15,9 +16,8 @@ class Solution:
                 if cycle(c,seen):
                     return True
             seen.remove(course)
-            graph[course]= []
+            graph[course] = []
             return False
-        
 
         for n in range(numCourses):
             if cycle(n,seen):

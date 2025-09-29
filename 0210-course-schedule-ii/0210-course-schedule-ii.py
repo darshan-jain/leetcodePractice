bf34c0f9e59772dea29f22b02692d29806049e93
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-29 01:02:50 -0700

    0210-COURSE-SCHEDULE-II Stats: Time: 7 ms (26.15%), Space: 19.6 MB (29.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,58 +1,31 @@
 from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-
         graph = defaultdict(list)
         for a,b in prerequisites:
             graph[a].append(b)
-        
-        seen = set()
-        flag = 0 
-        
-        def cycle(course,seen):
-            if course in seen:
+        res = []
+        UNVISITED = 0 
+        VISITING = 1
+        VISITED = 2
+        state = [UNVISITED for _ in range(numCourses)]
+        def dfs(node):
+            if state[node]==VISITING:
+                return False 
+            if state[node]==VISITED:
                 return True
-            seen.add(course)
-            for c in graph[course]:
-                if cycle(c,seen):
-                    return True
-            seen.remove(course)
-            graph[course] = []
-            return False
+            state[node] = VISITING 
+            for nei in graph[node]:
+                if dfs(nei) == False:
+                    return False
+            state[node] = VISITED 
+            res.append(node)
+            return True
 
         
+
         for i in range(numCourses):
-            if cycle(i,seen):
-                flag=1 # not possible
-        
-        if flag==1:
-            return []
-
-        #findorder 
-
-        adj = defaultdict(list)
-        st = []
-
-        for a,b in prerequisites:
-            adj[b].append(a)
-        vis = [0 for _ in range(numCourses)]
-        
-        def dfs(node,vis,adj,st):
-            vis[node]=1
-            for nei in adj[node]:
-                if vis[nei]!=1:
-                    dfs(nei, vis,adj,st)
-            st.append(node)
-
-        
-        for i in range(numCourses):
-            if vis[i]!=1:
-                dfs(i,vis,adj,st)
-        
-        ts = []
-        while st:
-            ts.append(st.pop())
-        return ts
-
-
+            if dfs(i)==False:
+                return []
+        return res 
         

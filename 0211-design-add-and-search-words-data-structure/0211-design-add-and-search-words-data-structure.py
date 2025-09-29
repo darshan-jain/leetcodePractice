d8cd20c501ef56f35a4ac86cfe6f62301b3fe4bb
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-28 19:27:11 -0700

    0211-DESIGN-ADD-AND-SEARCH-WORDS-DATA-STRUCTURE Stats: Time: 1297 ms (51.39%), Space: 65.5 MB (62.2%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.isWord = False
+        self.word = False
 class WordDictionary:
 
     def __init__(self):
@@ -13,7 +13,7 @@ class WordDictionary:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.isWord = True
+        cur.word = True
         
 
     def search(self, word: str) -> bool:
@@ -21,20 +21,18 @@ class WordDictionary:
             cur = root
             for i in range(j,len(word)):
                 c = word[i]
-                if c==".":
+                if c ==".":
                     for child in cur.children.values():
-                        if dfs(i+1,child):
+                        if dfs(i+1, child):
                             return True
                     return False
-                        
                 else:
                     if c not in cur.children:
                         return False
                     cur = cur.children[c]
-            return cur.isWord
+            return cur.word
         return dfs(0,self.root)
-        
-
+                
         
 
 

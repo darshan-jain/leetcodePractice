7111753447a569aa5f93af2d9fa82ae489534e1b
Darshan Manoj <darshandhokajain@gmail.com>
2025-09-29 23:46:51 -0700

    0332-RECONSTRUCT-ITINERARY Stats: Time: 3 ms (71.14%), Space: 17.9 MB (85.27%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,8 +1,7 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = defaultdict(list)
-
-        for src,dest in sorted(tickets)[::-1]:
+        for src, dest in sorted(tickets, reverse = True):
             graph[src].append(dest)
         
         stack = ["JFK"]
@@ -12,6 +11,5 @@ class Solution:
             while graph[stack[-1]]:
                 stack.append(graph[stack[-1]].pop())
             itenary.append(stack.pop())
-        return list(reversed(itenary))
-
+        return itenary[::-1]
         

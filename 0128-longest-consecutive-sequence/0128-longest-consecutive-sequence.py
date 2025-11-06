6efa5e2f16a8eae010716521fb1af7f5e5b591f9
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-06 02:40:39 -0800

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,7 +2,6 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums = set(nums)
         maxLen = 0 
-
         for num in nums:
             curNum = num
             curLen = 1
@@ -10,6 +9,7 @@ class Solution:
                 while curNum+1 in nums:
                     curLen+=1
                     curNum+=1
-                maxLen = max(maxLen, curLen)
+                    maxLen = max(maxLen, curLen)
         return maxLen
+
         

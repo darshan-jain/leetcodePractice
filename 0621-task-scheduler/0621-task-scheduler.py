340c12fe3ba9a1e06a351a09068667728d1af0cc
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-08 11:26:26 -0800

    0621-TASK-SCHEDULER Stats: Time: 147 ms (35.86%), Space: 19.2 MB (64.86%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,22 +1,19 @@
 import heapq
+from collections import deque
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
         maxheap = [-cnt for cnt in count.values()]
         heapq.heapify(maxheap)
-
-        time = 0 
+        time =0 
         q = deque()
-
-        while q or maxheap:
+        while maxheap or q:
             time+=1
             if maxheap:
                 val = 1+heapq.heappop(maxheap)
-                if val!=0:
-                    q.append([val, time+n])
+                if val:
+                    q.append((val,time+n))
             if q and q[0][1]==time:
                 heapq.heappush(maxheap, q.popleft()[0])
         return time
-
-
         

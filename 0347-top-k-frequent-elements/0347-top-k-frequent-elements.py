4d174900c788918f12e37faaf2fcaaa78bb07b1e
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-09 12:52:50 -0800

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 0 ms (100%), Space: 21.3 MB (61.42%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,15 @@
 import heapq
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        d = {}
-        for num in nums:
-            d[num] = 1+ d.get(num,0)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count  =Counter(nums)
+        maxheap = []
+        for a,v in count.items():
+            heapq.heappush(maxheap, (-v,a))
         
-        maxHeap = []
-        for val, count in d.items():
-            heapq.heappush(maxHeap, (-count,val))
-        res= []
-        while k >0:
-            count,val1 = heapq.heappop(maxHeap)
-            res.append(val1)
+        res = []
+        while k>0:
+            freq, val = heapq.heappop(maxheap)
+            res.append(val)
             k-=1
         return res
+        

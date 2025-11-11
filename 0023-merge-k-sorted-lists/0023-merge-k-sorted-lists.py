584e87e1c470dc02457556ccb633532cd69def0e
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-11 10:01:12 -0800

    0023-MERGE-K-SORTED-LISTS Stats: Time: 8 ms (75.87%), Space: 20.2 MB (55.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,7 +7,7 @@ import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         dummy = ListNode(0)
-        tail = dummy
+        tail = dummy 
         minheap = []
         for i in range(len(lists)):
             node = lists[i]
@@ -18,6 +18,6 @@ class Solution:
             tail.next = node
             tail = tail.next
             if node.next:
-                heapq.heappush(minheap, (node.next.val,i,node.next))
+                heapq.heappush(minheap,(node.next.val,i,node.next))
         return dummy.next
         

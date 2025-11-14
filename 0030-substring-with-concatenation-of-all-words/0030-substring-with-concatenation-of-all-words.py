18f9c3c1efdbdeb6f96d6ea4bdd0ad448c6cb51b
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-13 23:21:04 -0800

    0030-SUBSTRING-WITH-CONCATENATION-OF-ALL-WORDS Stats: Time: 9022 ms (6.85%), Space: 18.8 MB (38.07%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -2,24 +2,24 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words:
             return []
+        
         totwords = len(words)
-        wordlen = len(words[0])
-        winsize = totwords * wordlen
-        freq_cnt = Counter(words)
+        wordsize = len(words[0])
+        winsize = totwords * wordsize
+        freq_count = Counter(words)
         res = []
 
-        for i in range(len(s)-winsize+1):
+        for i in range(len(s)- winsize+1):
             curr_freq = {}
             j = i 
-            while j < i+winsize:
-                curr_word = s[j:j+wordlen]
-                if curr_word not in freq_cnt:
+            while j< i + winsize:
+                curr_word = s[j:j+wordsize]
+                if curr_word not in freq_count:
                     break
-                curr_freq[curr_word] = 1+curr_freq.get(curr_word,0)
-
-                if curr_freq[curr_word] > freq_cnt[curr_word]:
+                curr_freq[curr_word] = 1+ curr_freq.get(curr_word,0)
+                if curr_freq[curr_word] > freq_count[curr_word]:
                     break
-                j+=wordlen
+                j+=wordsize
             if j==i+winsize:
                 res.append(i)
         return res

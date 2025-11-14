200ebb4fa2cd05a96570e85bd29206539c14e2f7
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-14 11:31:44 -0800

    0632-SMALLEST-RANGE-COVERING-ELEMENTS-FROM-K-LISTS Stats: Time: 283 ms (42.58%), Space: 38.8 MB (34.74%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -3,21 +3,20 @@ class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         heap = []
         currmax = float("-inf")
-
         for i in range(len(nums)):
             val = nums[i][0]
             heapq.heappush(heap, (val,i,0))
-            currmax = max(currmax, val)
-        bleft, bright = float("-inf"), float("inf")
+            currmax  = max(currmax, val)
+        bleft,bright = float("-inf"), float("inf")
 
         while heap:
-            val, listidx, elemidx = heapq.heappop(heap)
+            val, listidx,elemidx = heapq.heappop(heap)
             if currmax - val < bright - bleft:
-                bleft,bright = val, currmax
-            if elemidx+1==len(nums[listidx]):
+                bleft,bright = val, currmax 
+            if elemidx+1>=len(nums[listidx]):
                 break
             nextval = nums[listidx][elemidx+1]
-            heapq.heappush(heap, (nextval,listidx,elemidx+1))
+            heapq.heappush(heap, (nextval, listidx, elemidx+1))
             currmax = max(currmax, nextval)
         return [bleft,bright]
         

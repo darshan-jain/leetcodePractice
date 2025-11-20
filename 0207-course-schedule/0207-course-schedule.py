f856d1b705d87caa6750d937b3b47e89fe5f0f2a
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-20 11:11:48 -0800

    0207-COURSE-SCHEDULE Stats: Time: 6 ms (51.72%), Space: 19.4 MB (31.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,22 @@
-from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
         for a,b in prerequisites:
             graph[a].append(b)
-        
-        def cycle(course,seen):
+
+        def cycle(course, seen):
             if course in seen:
                 return True
             seen.add(course)
             for nei in graph[course]:
-                if cycle(nei,seen):
+                if cycle(nei, seen):
                     return True
-            seen.remove(course)
             graph[course]=[]
+            seen.remove(course)
             return False
-        
-
         seen = set()
         for i in range(numCourses):
             if cycle(i,seen):
-                return False 
+                return False
         return True
         

21b1e45fbad193890ec10287eb366ff7e19103cb
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-20 11:07:06 -0800

    0310-MINIMUM-HEIGHT-TREES Stats: Time: 31 ms (99.17%), Space: 29.9 MB (31.8%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -2,7 +2,6 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n<=2:
             return [x for x in range(n)]
-        
         neighbors = [set() for _ in range(n)]
         for start, end in edges:
             neighbors[start].add(end)
@@ -12,7 +11,6 @@ class Solution:
         for i in range(n):
             if len(neighbors[i])==1:
                 leaves.append(i)
-        
         remnodes = n
         while remnodes>2:
             remnodes-=len(leaves)

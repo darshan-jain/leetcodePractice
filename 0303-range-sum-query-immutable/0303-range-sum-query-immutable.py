db46d44ca546d5c19bace330df6916675e88cb74
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-21 13:15:33 -0800

    0303-RANGE-SUM-QUERY-IMMUTABLE Stats: Time: 0 ms (100%), Space: 21.9 MB (26.56%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,14 +1,27 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.vals = nums
-        
+        self.prefix = []
+        for i,num in enumerate(nums):
+            if i==0:
+                self.prefix.append(num)
+            else:
+                self.prefix.append(self.prefix[-1]+num)
+                
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.vals[left:right+1])
+        leftSum = self.prefix[left-1] if left>0 else 0 
+        rightSum = self.prefix[right]
+        return rightSum-leftSum
         
 
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+
+#optimised approach would be to actually store the prefix sum so that left sum and right sum can be got individually 
+# rightSum = self.prefix[right]
+# leftSum = self.prefix[left-1] if left>0 else 0 
+# return rightSum - leftSum

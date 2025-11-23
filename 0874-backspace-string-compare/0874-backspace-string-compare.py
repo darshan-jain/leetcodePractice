b354f05d187830569ead55e73ac5f571f57097e1
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-23 11:39:47 -0800

    0874-BACKSPACE-STRING-COMPARE Stats: Time: 0 ms (100%), Space: 17.8 MB (35.87%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -3,23 +3,16 @@ class Solution:
         stacks = []
         stackt = []
         for c in s:
-            if c=="#":
+            if c=='#':
                 if len(stacks)>0:
                     stacks.pop()
-                else:
-                    continue
             else:
                 stacks.append(c)
-
         for c in t:
-            if c=="#":
+            if c=='#':
                 if len(stackt)>0:
                     stackt.pop()
-                else:
-                    continue
             else:
                 stackt.append(c)
-        return stacks == stackt
-
-            
+        return stacks==stackt
         

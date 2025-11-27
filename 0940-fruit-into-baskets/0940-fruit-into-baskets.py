349793e415c22d171a7ec5d4f1937d21270d9b89
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-27 12:53:17 -0800

    0940-FRUIT-INTO-BASKETS Stats: Time: 203 ms (43.3%), Space: 23.7 MB (40.84%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,15 +1,13 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        #hashmap for baskets 
         hm = {}
-        left = 0 
+        left=0 
         right = 0 
         maxfruits = 0 
 
         for right in range(0,len(fruits)):
-            hm[fruits[right]]=1+hm.get(fruits[right], 0)
+            hm[fruits[right]] = 1+ hm.get(fruits[right], 0)
 
-            #sliding window - to remove elements from left 
             while len(hm)>2:
                 fruitcount = hm[fruits[left]]
                 if fruitcount==1:
@@ -17,8 +15,7 @@ class Solution:
                 else:
                     hm[fruits[left]]-=1
                 left+=1
-
+            
             maxfruits = max(maxfruits, right-left+1)
         return maxfruits
-
         

4b461baf31ab0388e2c083651637eb11982e4d8e
Darshan Manoj <darshandhokajain@gmail.com>
2025-11-27 12:30:18 -0800

    0567-PERMUTATION-IN-STRING Stats: Time: 11 ms (97.2%), Space: 17.8 MB (87.44%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dic1 = [0] * 26      
-        dic2 = [0]*26
+        dic1 = [0]*26 
+        dic2 = [0]*26 
         s1Len = len(s1)
         s2Len = len(s2)
+        left = 0 
+        right = 0 
         if s1Len > s2Len:
             return False 
-        left = 0
-        right = 0 
-        while right<s1Len:
+        while right < s1Len:
             dic1[ord(s1[right]) - ord('a')]+=1
             dic2[ord(s2[right]) - ord('a')]+=1
             right+=1
         
         right-=1
-        while right<s2Len:
+        while right!=s2Len:
             if dic1==dic2:
                 return True
             right+=1
-            if right!=s2Len:
-                dic2[ord(s2[right]) - ord('a')]+=1
+            if right<s2Len:
+                dic2[ord(s2[right])- ord('a')]+=1
             dic2[ord(s2[left]) - ord('a')]-=1
             left+=1
         return False
-            
-
-
-
-       
+        

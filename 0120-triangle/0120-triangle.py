b1db50bdb17dc67969e918beefa55abef59e0f3b
Darshan Manoj <darshandhokajain@gmail.com>
2025-12-08 18:39:42 -0800

    0120-TRIANGLE Stats: Time: 0 ms (100%), Space: 19.6 MB (20.52%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,9 +1,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [0] * (len(triangle)+1)
-
-        for row in triangle[::-1]:
-            for i, n in enumerate(row):
-                dp[i] = n + min(dp[i],dp[i+1])
+        height = len(triangle)
+        dp = [[0]*(height+1) for _ in range(height+1)]
+        for level in range(height-1, -1, -1):
+            for i in range(0,level+1):
+                dp[level][i] = triangle[level][i] + min(dp[level+1][i], dp[level+1][i+1])
         
-        return dp[0]
+        return dp[0][0]
+        

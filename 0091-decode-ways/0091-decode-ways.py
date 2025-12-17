0130e0989c18fb4da4cc4f94c325911a0d5f1911
Darshan Manoj <darshandhokajain@gmail.com>
2025-12-16 22:31:48 -0800

    0091-DECODE-WAYS Stats: Time: 20 ms (3.3%), Space: 18 MB (29.59%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,14 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0]=='0':
-            return 0 
-        dp = [1,1]
-
-        for ind,num in enumerate(s[1:],2):
-            ways =0 
-            if num!='0':
-                ways+=dp[ind-1]
-            if 10<=int(s[ind-2] + num)<=26:
-                ways+=dp[ind-2]
-            dp.append(ways)
-        return dp[-1]
+        n = len(s)
+        ways = [0]*(n+1)
+        ways[0] = 1
+        for i in range(len(s)):
+            print(s[i],ways)
+            if s[i]=='0':
+                ways[i+1]+=0
+            if 1<= int(s[i]) <=9:
+                ways[i+1]+=ways[i]
+            if i>=1 and 10<= int(s[i-1])*10 + int(s[i]) <=26:
+                ways[i+1]+=ways[i-1]
+        
+        return ways[-1]
+        

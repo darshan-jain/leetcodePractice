65154f065d2025e6d804aec80342734ff898aa51
Darshan Manoj <darshandhokajain@gmail.com>
2025-12-17 16:45:03 -0800

    0416-PARTITION-EQUAL-SUBSET-SUM Stats: Time: 615 ms (56.24%), Space: 20.1 MB (45.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,18 +1,16 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        #base case
-        if sum(nums)%2:  #sum is odd , cannot be partitioned
+        if sum(nums)%2:
             return False
+        target = sum(nums)//2
         dp = set()
         dp.add(0)
-        target = sum(nums)//2
 
         for i in range(len(nums)-1,-1,-1):
-            nextDP = set()
+            nextdp = set()
             for t in dp:
-                nextDP.add(t+nums[i])
-                nextDP.add(t)
-            dp = nextDP
-        
+                nextdp.add(t+nums[i])
+                nextdp.add(t)
+            dp = nextdp
         return True if target in dp else False
         

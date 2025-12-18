c59d09f10e405e5feaa2cba55da88d95946dd138
Darshan Manoj <darshandhokajain@gmail.com>
2025-12-17 21:43:51 -0800

    0698-PARTITION-TO-K-EQUAL-SUM-SUBSETS Stats: Time: 141 ms (40.23%), Space: 17.2 MB (100%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,37 +1,26 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
+        if sum(nums)%k:
+            return False 
         nums.sort(reverse=True)
-        total = sum(nums)
-        if total % k != 0:
-            return False
-        
-        target = int(total / k)
-        used = [False] * n
-        
-        def dfs(index, total, k): 
-            
-            if k == 0:
+        target = sum(nums)//k
+        used = [False] * len(nums)
+
+        def backtrack(i,k,subsetSum):
+            if k==0:
                 return True
-            
-            if total == 0:
-                return dfs(0, target, k - 1)
-            
-            for i in range(index, n):
-                
-                if i > 0 and not used[i - 1] and nums[i] == nums[i - 1]:
+            if subsetSum==target:
+                return backtrack(0,k-1,0)
+            for j in range(i,len(nums)):
+                if j>0 and not used[j-1] and nums[j]==nums[j-1]:
                     continue
-                    
-                if used[i] or total - nums[i] < 0:
+                if used[j] or subsetSum + nums[j] > target:
                     continue
-                
-                used[i] = True
-                if dfs(i + 1, total - nums[i], k):
+                used[j] = True
+                if backtrack(j+1, k,subsetSum + nums[j]):
                     return True
-                used[i] = False
+                used[j] = False
             return False
-        
-        return dfs(0, target, k)
-        
-        
+
+        return backtrack(0,k,0)
         

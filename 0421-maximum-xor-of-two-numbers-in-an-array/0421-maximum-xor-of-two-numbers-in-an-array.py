00813ec9ada99b054b2f2f5f42676941cd149cec
Darshan Manoj <darshandhokajain@gmail.com>
2025-12-21 18:20:40 -0800

    0421-MAXIMUM-XOR-OF-TWO-NUMBERS-IN-AN-ARRAY Stats: Time: 9721 ms (5.03%), Space: 771.3 MB (5.03%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -6,31 +6,33 @@ class Solution:
     def __init__(self):
         self.root = TrieNode()
 
-    def insertbits(self, num):
-        bits = bin(num)[2:].zfill(32)
+    def insertbits(self,num):
+        bits= bin(num)[2:].zfill(32)
         node = self.root 
         for bit in bits:
             if bit not in node.children:
                 node.children[bit] = TrieNode()
             node = node.children[bit]
     
-    def findmaxXOR(self, num):
-        bits = bin(num)[2:].zfill(32)
+    def findmax(self,num):
+        bits= bin(num)[2:].zfill(32)
         node = self.root 
         maxxor = ''
         for bit in bits:
-            if bit =='0':
-                oppo_bit='1'
-            elif bit=='1':
-                oppo_bit ='0'
+            if bit =='1':
+                oppo_bit = '0'
+            else:
+                oppo_bit = '1'
             
             if oppo_bit in node.children:
-                maxxor += oppo_bit 
+                maxxor+=oppo_bit 
                 node = node.children[oppo_bit]
             else:
-                maxxor += bit 
+                maxxor+=bit
                 node = node.children[bit]
         return int(maxxor,2) ^ num
+
+
 
     def findMaximumXOR(self, nums: List[int]) -> int:
         for num in nums:
@@ -38,5 +40,6 @@ class Solution:
         
         max_ = 0 
         for num in nums:
-            max_ = max(max_, self.findmaxXOR(num))
+            max_ = max(max_, self.findmax(num))
         return max_
+        

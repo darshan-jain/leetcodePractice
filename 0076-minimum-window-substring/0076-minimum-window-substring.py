1740708947177e6ec6037ae0d482121dcd01e38d
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-11 13:23:00 -0800

    0076-MINIMUM-WINDOW-SUBSTRING Stats: Time: 155 ms (23.06%), Space: 19.8 MB (7.78%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,9 +1,10 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         res = ""
+        l = 0 
+        r = 0 
         c = Counter(t)
         overall = len(c)
-        l=r=0 
         while r < len(s):
             if s[r] in c:
                 c[s[r]]-=1
@@ -12,7 +13,7 @@ class Solution:
             while overall==0:
                 if not res:
                     res = s[l:r+1]
-                elif len(res)>(r-l+1):
+                elif len(res) > (r-l+1):
                     res = s[l:r+1]
                 if s[l] in c:
                     c[s[l]]+=1
@@ -21,6 +22,5 @@ class Solution:
                 l+=1
             r+=1
         return res
-            
 
         

35b691339bfe7f77dddd293cc9c216cd6239c9c9
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-13 12:15:30 -0800

    0084-LARGEST-RECTANGLE-IN-HISTOGRAM Stats: Time: 234 ms (12.94%), Space: 33.3 MB (34.1%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,17 +1,17 @@
 class Solution:
-    def largestRectangleArea(self, height: List[int]) -> int:
-        maxarea = 0
-        stack = []
-        n = len(height)
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxarea = 0 
+        n = len(heights)
         left = [0]*n
         right = [0]*n
+        stack = []
 
         for i in range(n):
             if not stack:
                 left[i] = 0 
                 stack.append(i)
             else:
-                while stack and height[stack[-1]]>=height[i]:
+                while stack and heights[stack[-1]]>=heights[i]:
                     stack.pop()
                 left[i] = 0 if not stack else stack[-1]+1
                 stack.append(i)
@@ -19,16 +19,15 @@ class Solution:
         stack = []
         for i in range(n-1,-1,-1):
             if not stack:
-                right[i] = n-1
+                right[i] = n-1 
                 stack.append(i)
             else:
-                while stack and height[stack[-1]]>=height[i]:
+                while stack and heights[stack[-1]]>=heights[i]:
                     stack.pop()
                 right[i] = n-1 if not stack else stack[-1]-1
                 stack.append(i)
-        
-        for i in range(n):
-            maxarea = max(maxarea, height[i]*(right[i] - left[i] + 1) )
-        return maxarea
 
+        for i in range(n):
+            maxarea = max(maxarea, heights[i]* (right[i]-left[i]+1))
+        return maxarea
         

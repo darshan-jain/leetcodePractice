7673512f2154056db11666411fd5d431269ce882
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-13 11:29:46 -0800

    0394-DECODE-STRING Stats: Time: 0 ms (100%), Space: 19.4 MB (7.56%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,20 +1,28 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+
+        # 3[a2[c]]
+        # stack = ['3','[','a',]
+        # curr = 'c'
+        # num = 2 
+
+
         stack = []
-        
 
         for c in s:
-            if c !=']':
+            if c!=']':
                 stack.append(c)
             else:
                 curr=""
                 while stack and stack[-1]!='[':
-                    curr = stack.pop()+curr
+                    curr = stack.pop() + curr
                 stack.pop()
                 num = ""
                 while stack and stack[-1].isdigit():
-                    num = stack.pop()+num
-                stack.append(curr*int(num))
+                    num = stack.pop() + num
+                stack.append(curr * int(num))
         return "".join(stack)
+
+                
 
         

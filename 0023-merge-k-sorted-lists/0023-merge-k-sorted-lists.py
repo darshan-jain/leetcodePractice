85d686bc4c9a50e23eb66e9a9eb4b6e711edabcb
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-14 22:54:58 -0800

    0023-MERGE-K-SORTED-LISTS Stats: Time: 7 ms (90.16%), Space: 23.3 MB (6.73%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,23 +1,26 @@
+import heapq
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        minheap = []
         dummy = ListNode(0)
         tail = dummy 
-        minheap = []
         for i in range(len(lists)):
-            node = lists[i]
+            node = lists[i] #here since question says array and input params is ListNode - we choose ListNode directly 
             if node:
-                heapq.heappush(minheap, (node.val,i,node))
+                heapq.heappush(minheap,(node.val,i,node))
+        
         while minheap:
             val,i,node = heapq.heappop(minheap)
-            tail.next = node
-            tail = tail.next
+            tail.next = node 
+            tail = tail.next 
             if node.next:
                 heapq.heappush(minheap,(node.next.val,i,node.next))
         return dummy.next
+
         

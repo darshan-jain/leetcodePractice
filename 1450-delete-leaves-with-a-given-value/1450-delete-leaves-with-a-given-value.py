5ca7b0a97e5ad413d08d3fda674585cc5d25098b
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-16 08:39:31 -0800

    1450-DELETE-LEAVES-WITH-A-GIVEN-VALUE Stats: Time: 3 ms (40.59%), Space: 19.8 MB (16.69%) - LeetHub

--- 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.py
@@ -6,39 +6,49 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        if root is None:
-            return None
-        if root.left is None and root.right is None and root.val==target:
-            return None
-        def search(root):
-            if root is None:
-                return False 
-            if root.left is None and root.right is None and root.val==target:
-                return True
-            return search(root.left) or search(root.right)
+        # if root is None:
+        #     return None
+        # if root.left is None and root.right is None and root.val==target:
+        #     return None
+        # def search(root):
+        #     if root is None:
+        #         return False 
+        #     if root.left is None and root.right is None and root.val==target:
+        #         return True
+        #     return search(root.left) or search(root.right)
         
-        def dfs(par,root,le):
-            if par is None and root.left is None and root.right is None and root.val ==target:
-                return None
-            if par is None and root is None:
-                return
-            if par is not None and root is None:
-                return 
-            if par is not None and root is not None and root.left is None and root.right is None and root.val==target:
-                if le==1:
-                    par.left = None
-                else:
-                    par.right = None
-            dfs(root,root.left,1)
-            dfs(root,root.right,0)
-            return 5
+        # def dfs(par,root,le):
+        #     if par is None and root.left is None and root.right is None and root.val ==target:
+        #         return None
+        #     if par is None and root is None:
+        #         return
+        #     if par is not None and root is None:
+        #         return 
+        #     if par is not None and root is not None and root.left is None and root.right is None and root.val==target:
+        #         if le==1:
+        #             par.left = None
+        #         else:
+        #             par.right = None
+        #     dfs(root,root.left,1)
+        #     dfs(root,root.right,0)
+        #     return 5
         
-        while search(root):
-            val = dfs(None, root,1)
-            if val==None:
-                root = None
-                break
+        # while search(root):
+        #     val = dfs(None, root,1)
+        #     if val==None:
+        #         root = None
+        #         break
 
         
+        # return root
+
+        if not root:
+            return None
+        
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right,target)
+
+        if not root.left and not root.right and root.val==target:
+            return None
         return root
         

6fdc40e57bd7b8d1b253a6b70363238ddee1e3b8
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-24 14:50:26 -0800

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 18 MB (36.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,25 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.cache={}
     def climbStairs(self, n: int) -> int:
-
-        #base cases 
-        # n= 0 -> 1 way 
-        # n = 1 -> 1 way 
-        # n = 2 - > 2 ways 1+1 or 2
-        # recursive tree 
-        # n=4 
-        # you can come to 3 or 2 from 4
-        # you can come to 1 or 2 from 2 and 1 or 0 from 2
-        if n==0 or n==1:
+        if n==0:
             return 1
-        if n==2:
-            return 2
-        if n in self.cache:
-            return self.cache[n]
-        res = self.climbStairs(n-1) + self.climbStairs(n-2)
-        self.cache[n] = res
-        return res
+        if n<=2:
+            return n 
+        dp = [0]*(n+2)
+        dp[1] = 1
+        for i in range(2,n+2):
+            dp[i] = dp[i-1]+dp[i-2]
+        print(dp)
+        return dp[-1]
 
-        
         

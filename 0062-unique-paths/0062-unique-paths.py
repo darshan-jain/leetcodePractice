ed40371dbe38b2d9f3eb2ce544f6b69cfd9e95d1
Darshan Manoj <darshandhokajain@gmail.com>
2026-01-25 08:50:06 -0800

    0062-UNIQUE-PATHS Stats: Time: 3 ms (23.25%), Space: 19.4 MB (23.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #recursive soln is exhaustive 
-        if m==n==1:
-            return 1 
-        dp = [[0]*n]*m
-        for i in range(m):
-            dp[i][0]=1
-        for j in range(n):
-            dp[0][j]=1
+        grid = [[1]*n for _ in range(m)]
+
         for i in range(1,m):
             for j in range(1,n):
-                dp[i][j] = dp[i-1][j]+dp[i][j-1]
-        return dp[-1][-1]
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        
+        return grid[m-1][n-1]
         

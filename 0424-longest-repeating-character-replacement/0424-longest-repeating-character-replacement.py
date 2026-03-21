d8cc0b4537ec0ca58395d0ed67d979784864dc16
Darshan Manoj <darshandhokajain@gmail.com>
2026-03-21 15:09:42 -0700

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 91 ms (48.64%), Space: 19.6 MB (75.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,15 @@ class Solution:
         hm = {}
         l = 0 
         r = 0 
-        majority = 0 
         maxLen = 0 
-        while r<len(s):
+        majority = 0 
+        while r < len(s):
             hm[s[r]]=1+hm.get(s[r],0)
-            majority = max(majority,hm[s[r]])
+            majority = max(majority, hm[s[r]])
             while r-l+1 > majority+k:
                 hm[s[l]]-=1
                 l+=1
             maxLen = max(maxLen, r-l+1)
             r+=1
         return maxLen
-
         

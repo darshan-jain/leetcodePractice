1ee0c0f212effd6ccc6dae4b37e26c6827324a7e
Darshan Manoj <darshandhokajain@gmail.com>
2026-03-27 22:10:03 -0700

    0040-COMBINATION-SUM-II Stats: Time: 0 ms (100%), Space: 19.5 MB (60.64%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,19 +1,18 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = [] 
         candidates.sort()
+        res = []
+
         def comb(path, start, target):
             if target==0:
                 res.append(path[:])
                 return 
             for i in range(start, len(candidates)):
                 if candidates[i] > target:
-                    continue
+                    break
                 if i>start and candidates[i]==candidates[i-1]:
                     continue
-                comb(path + [candidates[i]], i+1, target - candidates[i])
-
-                
+                comb(path + [candidates[i]], i+1, target-candidates[i])
         
         comb([],0,target)
         return res

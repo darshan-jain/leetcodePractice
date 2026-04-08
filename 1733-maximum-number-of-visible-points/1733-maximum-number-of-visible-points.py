7290ae039ba4aac5faacd138134d5891c8ce1ba5
Darshan Manoj <darshandhokajain@gmail.com>
2026-04-08 16:45:47 -0700

    1733-MAXIMUM-NUMBER-OF-VISIBLE-POINTS Stats: Time: 286 ms (40.07%), Space: 46.3 MB (47.09%) - LeetHub

--- 1733-maximum-number-of-visible-points/1733-maximum-number-of-visible-points.py
+++ 1733-maximum-number-of-visible-points/1733-maximum-number-of-visible-points.py
@@ -1,30 +1,28 @@
 import math
-from typing import List
-
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
-        samept = 0
+        samept = 0 
         angles = []
-
-        lx, ly = location
-
-        for x, y in points:
-            if x == lx and y == ly:
-                samept += 1
-            else:
-                dx = x - lx
-                dy = y - ly
-                ang = math.degrees(math.atan2(dy, dx))
-                angles.append(ang)
-
+        for x,y in points:
+            diffx = x - location[0]
+            diffy = y - location[1]
+            if diffx==0 and diffy==0:
+                samept+=1
+                continue
+            angles.append(math.degrees(math.atan2(diffy,diffx)))
+        
         angles.sort()
-        temp = angles + [a + 360 for a in angles]
+        temp = angles + [a+360 for a in angles]
 
-        l = 0
-        res = 0
-        for r in range(len(temp)):
-            while temp[r] - temp[l] > angle:
-                l += 1
-            res = max(res, r - l + 1)
-
+        l= 0 
+        r = 0 
+        res = 0 
+        while r < len(temp):
+            while temp[r]- temp[l] > angle:
+                l+=1
+            res = max(res, r-l+1)
+            r+=1
         return res + samept
+
+
+        

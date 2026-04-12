95f7916732c9656060e388852f5eecc6f1ce0ed2
Darshan Manoj <darshandhokajain@gmail.com>
2026-04-11 20:29:35 -0700

    0304-RANGE-SUM-QUERY-2D-IMMUTABLE Stats: Time: 117 ms (60.14%), Space: 33.6 MB (58.06%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,17 +1,17 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        r = len(matrix)
-        c = len(matrix[0])
-        self.mat = [[0]*(c+1) for _ in range(r+1)]
-        for i in range(1,r+1):
-            for j in range(1,c+1):
-                self.mat[i][j] = self.mat[i-1][j] + self.mat[i][j-1] - self.mat[i-1][j-1] + matrix[i-1][j-1]
-        print(self.mat)
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self.mat = [[0]*(cols+1) for _ in range(rows+1)]
+        for i in range(1, rows+1):
+            for j in range(1,cols+1):
+                self.mat[i][j] = self.mat[i-1][j] + self.mat[i][j-1]  - self.mat[i-1][j-1] + matrix[i-1][j-1]
+        
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        val = self.mat[row2+1][col2+1] - self.mat[row2+1][col1] - self.mat[row1][col2+1] +self.mat[row1][col1]
+        val = self.mat[row2+1][col2+1] - self.mat[row1][col2+1] - self.mat[row2+1][col1] +self.mat[row1][col1]
         return val
         
 

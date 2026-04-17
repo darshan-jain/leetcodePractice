dfd19e63f12f7973e752a35627e1e03707e3db58
Darshan Manoj <darshandhokajain@gmail.com>
2026-04-17 10:13:13 -0700

    0143-REORDER-LIST Stats: Time: 4 ms (29.58%), Space: 27.7 MB (97.66%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,39 +8,30 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #edge cases 
         if head is None:
-            return None
-        #find mid point
+            return None 
         slow = head 
         fast = head 
         while fast and fast.next:
             slow = slow.next 
-            fast = fast.next.next 
-        mid = slow 
-        l2 = mid.next
-        mid.next = None
-
-
-
-        #reverse the 2nd List 
-        curr = l2 
-        prev= None
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        l2 = prev
-
-        #Merge
-        l1 = head
+            fast = fast.next.next
+        mid = slow
+        l2 = mid.next 
+        mid.next = None 
+        prev = None 
+        while l2:
+            temp = l2.next 
+            l2.next = prev 
+            prev = l2
+            l2 = temp 
+        l2 = prev 
+        l1 = head 
         while l1 and l2:
-            l1next = l1.next
-            l2next = l2.next
-            l1.next = l2
-            l2.next = l1next
-            l1 = l1next
+            l1next = l1.next 
+            l2next = l2.next 
+            l1.next = l2 
+            l2.next = l1next 
+            l1 = l1next 
             l2 = l2next
         return head
         

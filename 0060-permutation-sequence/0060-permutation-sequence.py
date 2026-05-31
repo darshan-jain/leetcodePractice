1ea240edcb6144b7bb4376f9ad5ef0f2f4bdddd7
Darshan Manoj <darshandhokajain@gmail.com>
2026-05-31 12:15:49 -0700

    0060-PERMUTATION-SEQUENCE Stats: Time: 1559 ms (9.48%), Space: 19.2 MB (97.67%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,26 +1,26 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        arr = []
-        for i in range(1,n+1):
-            arr.append(i)
-        
-        res = []
-        sol = []
-        def backtrack():
-            if len(res)==k:
-                return
-            if len(sol) == len(arr):
-                res.append(sol[:])
-                return 
-            for num in arr:
-                if num not in sol:
-                    sol.append(num)
-                    backtrack()
-                    sol.pop()
-        
-        backtrack()
-        ans = ""
-        for elem in res[k-1]:
-            ans+=str(elem)
-        return ans
-        
+        nums = [i for i in range(1, n + 1)]
+
+        def next_permutation(arr):
+            i = len(arr) - 2
+
+            while i >= 0 and arr[i] >= arr[i + 1]:
+                i -= 1
+
+            if i >= 0:
+                j = len(arr) - 1
+                while arr[j] <= arr[i]:
+                    j -= 1
+                arr[i], arr[j] = arr[j], arr[i]
+
+            l, r = i + 1, len(arr) - 1
+            while l < r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l += 1
+                r -= 1
+
+        for _ in range(k - 1):
+            next_permutation(nums)
+
+        return "".join(map(str, nums))

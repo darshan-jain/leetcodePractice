a1722cfdaca6c196b40343694ec4afc4315459f3
Darshan Manoj <darshandhokajain@gmail.com>
2026-06-19 09:39:36 -0700

    0031-NEXT-PERMUTATION Stats: Time: 0 ms (100%), Space: 19.5 MB (26.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,28 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        def reverse(nums):
-            l =0 
-            h = len(nums)-1
-            while l<=h:
-                nums[l],nums[h] = nums[h], nums[l]
+        def rev(nums):
+            l = 0 
+            r = len(nums)-1
+            while l<=r:
+                nums[l],nums[r] = nums[r],nums[l]
                 l+=1
-                h-=1
+                r-=1
             return nums
 
-
-        i = len(nums)-2
+        n = len(nums)
+        i = n-2 
         while i>=0 and nums[i]>=nums[i+1]:
             i-=1
         
         if i>=0:
-            j = len(nums)-1
-            while j>i and nums[j] <= nums[i]:
+            j = n-1 
+            while j>i and nums[j]<=nums[i]:
                 j-=1
             
-            nums[i],nums[j] = nums[j],nums[i]
+            nums[i],nums[j] = nums[j], nums[i]
         
-        nums[i+1:] = reverse(nums[i+1:])
+        nums[i+1:] = rev(nums[i+1:])
         return nums
+
         

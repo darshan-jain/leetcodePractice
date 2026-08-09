b8728d36f4e3dc53e63b5f169c6b6c5dc2bdbec7
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-09 08:37:16 -0700

    0219-CONTAINS-DUPLICATE-II Stats: Time: 54 ms (14.28%), Space: 32.4 MB (74.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,13 +1,32 @@
+"""
+[1,2,3,1,2,3], k = 2
+        l   i
+set = [1,2]
+
+if nums[i] in set:
+    return True
+else:
+    add it to the set and remove nums[l] from set
+ 0,1,2,3
+[1,2,3,1], k = 3
+ l   r
+
+"""
+
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        win = set()
-        L = 0 
-        for R in range(len(nums)):
-            if R-L > k:
-                win.remove(nums[L])
-                L+=1
-            if nums[R] in win:
+        l = 0 
+        r = 0 
+        q = set()
+        for r in range(len(nums)):
+            if r-l > k:
+                q.remove(nums[l])
+                l+=1
+            if nums[r] in q:
                 return True
-            win.add(nums[R])
+            q.add(nums[r])
         return False
+            
+
         

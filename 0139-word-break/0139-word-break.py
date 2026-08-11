6e287dde87d363abdecbd30674bc85b21ccffa7c
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-11 11:59:09 -0700

    0139-WORD-BREAK Stats: Time: 7 ms (21.41%), Space: 19.5 MB (26.43%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,12 +1,20 @@
+# leetcode
+# 01234567
+
+#   i
+# T F F F T F F F T
+# 0 1 2 3 4 5 6 7 8
+
+
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)+1
-        dp = [False]*(n)
+        n = len(s)
+        dp = [False]*(n+1)
         dp[0] = True
-        for i in range(n):
-            for j in range(i):
+        for i in range(1,len(s)+1):
+            for j in range(0,i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
-                    break
         return dp[-1]
         

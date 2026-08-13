532d31cf0a7937217be4f32d9e25ae61a9ae0d96
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-13 14:53:08 -0700

    1422-DIVIDE-ARRAY-IN-SETS-OF-K-CONSECUTIVE-NUMBERS Stats: Time: 248 ms (65.25%), Space: 40.8 MB (56.23%) - LeetHub

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.py
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -1,20 +1,18 @@
 from collections import Counter
-from typing import List
-
 class Solution:
     def isPossibleDivide(self, nums: List[int], k: int) -> bool:
-        if len(nums) % k != 0:
-            return False
+        if len(nums)%k!=0:
+            return False 
         
         count = Counter(nums)
         sorted_keys = sorted(count.keys())
-        
+
         for num in sorted_keys:
             freq = count[num]
-            if freq > 0:
+            if freq>0:
                 for i in range(k):
-                    if count[num + i] < freq:
-                        return False
-                    count[num + i] -= freq
-                    
+                    if count[num+i] < freq:
+                        return False 
+                    count[num+i]-=freq
         return True
+        

d9c5c35f694c289d62613ece1997839418c1ed0d
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-13 13:21:48 -0700

    0874-BACKSPACE-STRING-COMPARE Stats: Time: 0 ms (100%), Space: 19.3 MB (23.86%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,16 +1,16 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-
-        i= len(s)-1
+        i = len(s)-1
         j = len(t)-1
         skip_s = 0 
         skip_t = 0 
         while i>=0 or j>=0:
+
             while i>=0:
                 if s[i]=="#":
                     skip_s+=1
                     i-=1
-                elif skip_s >0:
+                elif skip_s>0:
                     skip_s-=1
                     i-=1
                 else:
@@ -20,24 +20,19 @@ class Solution:
                 if t[j]=="#":
                     skip_t+=1
                     j-=1
-                elif skip_t >0:
+                elif skip_t>0:
                     skip_t-=1
                     j-=1
                 else:
                     break
+            
             if i>=0 and j>=0:
                 if s[i]!=t[j]:
                     return False 
             
             elif (i>=0) != (j>=0):
-                return False
-            
+                return False 
             i-=1
             j-=1
         return True
-
-            
-            
-
-        
         

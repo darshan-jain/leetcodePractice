0854c29ebbc1f97886a0daaaf5abec8467cecd09
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-15 10:26:05 -0700

    2414-MOVE-PIECES-TO-OBTAIN-A-STRING Stats: Time: 98 ms (62.39%), Space: 20.1 MB (58.48%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
@@ -1,56 +1,56 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        # i =0 
-        # j = 0 
-        # n = len(start)
-        # m = len(target)
-        # while i<n or i<m:
-        #     while i<n and start[i]=="_":
-        #         i+=1
-        #     while j<m and target[j]=="_":
-        #         j+=1
-            
-        #     if i==n or j==m:
-        #         return i==n and j==m
-            
-        #     if start[i]!=target[j]:
-        #         return False 
-        #     if start[i]=="L" and i<j:
-        #         return False 
-        #     if start[i]=="R" and i>j:
-        #         return False
-        #     i+=1
-        #     j+=1
-        
-        # return True
-
-        n = len(start)
-        result = target
-        m = len(result)
-        i = 0 
+        i =0 
         j = 0 
+        n = len(start)
+        m = len(target)
         while i<n or j<m:
-            while i<n and start[i]=='_':
+            while i<n and start[i]=="_":
                 i+=1
-            while j<m and result[j]=='_':
+            while j<m and target[j]=="_":
                 j+=1
             
-
-            #if end is reached
+            if i==n or j==m:
+                return i==n and j==m
             
-            if i == n or j == m:
-                return i == n and j == m
-            
-            
-            if start[i]!=result[j]:
+            if start[i]!=target[j]:
                 return False 
             if start[i]=="L" and i<j:
                 return False 
             if start[i]=="R" and i>j:
-                return False 
-            
+                return False
             i+=1
             j+=1
+        
         return True
+
+        # n = len(start)
+        # result = target
+        # m = len(result)
+        # i = 0 
+        # j = 0 
+        # while i<n or j<m:
+        #     while i<n and start[i]=='_':
+        #         i+=1
+        #     while j<m and result[j]=='_':
+        #         j+=1
+            
+
+        #     #if end is reached
+            
+        #     if i == n or j == m:
+        #         return i == n and j == m
+            
+            
+        #     if start[i]!=result[j]:
+        #         return False 
+        #     if start[i]=="L" and i<j:
+        #         return False 
+        #     if start[i]=="R" and i>j:
+        #         return False 
+            
+        #     i+=1
+        #     j+=1
+        # return True
         
         

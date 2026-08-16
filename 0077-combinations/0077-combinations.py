3db3b0938500a01d4990698f6fef10e851ca0219
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-16 13:17:54 -0700

    0077-COMBINATIONS Stats: Time: 141 ms (17.74%), Space: 61.3 MB (53.43%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,11 +2,11 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
 
-        def backtrack(i,sol ):
+        def backtrack(i, sol):
             if len(sol)==k:
                 res.append(sol[:])
                 return 
-            for j in range(i+1, n+1):
+            for j in range(i+1,n+1):
                 if j not in sol:
                     sol.append(j)
                     backtrack(j, sol)

a7b6983b3d96baa877de4d2409da58e7a9f23a3a
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-17 08:26:31 -0700

    0899-BINARY-GAP Stats: Time: 0 ms (100%), Space: 19.4 MB (36.82%) - LeetHub

--- 0899-binary-gap/0899-binary-gap.py
+++ 0899-binary-gap/0899-binary-gap.py
@@ -1,25 +1,14 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        binval = str(bin(n)[2:]) 
-        stack = []
-        right = []
-        for i in range(len(binval)-1,-1,-1):
-            
-            while stack and binval[stack[-1]]=="0":
-                stack.pop()
-
-            if stack:
-                right.append(stack[-1])
-            else:
-                right.append(-1)
-            stack.append(i)
-        right = right[::-1]
-        print(right)
-        res = [0]*len(right)
-        for i in range(len(right)):
-            if binval[i]=="1" and right[i]>0:
-                res[i]=right[i]-i
-        return max(res)
-
-        
+        binval = str(bin(n)[2:])
+        res = 0 
+        for i in range(len(binval)):
+            if binval[i]=="1":
+                j = i+1
+                while j<len(binval) and binval[j]=="0":
+                    j+=1
+                if j< len(binval) and binval[j]=="1":
+                    idxval = j-i
+                    res = max(res, idxval)
+        return res
         

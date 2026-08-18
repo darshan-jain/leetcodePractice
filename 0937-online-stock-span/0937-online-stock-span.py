5c8761f52cac41a994c42fb5ab119da6e7d22fa0
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-18 11:56:33 -0700

    0937-ONLINE-STOCK-SPAN Stats: Time: 52 ms (80.78%), Space: 24 MB (73.67%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -7,15 +7,13 @@ class StockSpanner:
     def next(self, price: int) -> int:
         if not self.stack:
             self.stack.append((price,1))
+            
         else:
-            if price < self.stack[-1][0]:
-                self.stack.append((price,1))
-            else:
-                newfreq = 1
-                while self.stack and price >= self.stack[-1][0]:
-                    val,freq = self.stack.pop()
-                    newfreq+=freq
-                self.stack.append((price,newfreq))
+            newfreq = 1 
+            while self.stack and price>=self.stack[-1][0]:
+                val,freq = self.stack.pop()
+                newfreq+=freq
+            self.stack.append((price, newfreq))
         return self.stack[-1][1]
         
 

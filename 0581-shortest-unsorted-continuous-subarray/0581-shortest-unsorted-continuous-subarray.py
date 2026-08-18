5a0abbad0e30adf03d3fceab221a365c51c97910
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-18 11:20:07 -0700

    0581-SHORTEST-UNSORTED-CONTINUOUS-SUBARRAY Stats: Time: 32 ms (6.75%), Space: 20.7 MB (24.07%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -1,21 +1,31 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        idx1=-1
-        new = [num for num in nums]
-        new.sort()
+
+        maxarr=[]
         for i in range(len(nums)):
-            if nums[i]!=new[i]:
+            if not maxarr:
+                maxarr.append(nums[i])
+            else:
+                maxarr.append(max(maxarr[-1],nums[i]))
+        print(maxarr)
+
+        minarr = []
+        for i in range(len(nums)-1,-1,-1):
+            if not minarr:
+                minarr.append(nums[i])
+            else:
+                minarr.append(min(minarr[-1], nums[i]))
+        minarr = minarr[::-1]
+        idx1=-1
+        for i in range(len(nums)):
+            if minarr[i]!=maxarr[i]:
                 idx1=i
                 break
-        
         idx2=len(nums)
         for i in range(len(nums)-1,-1,-1):
-           
-            if nums[i]!=new[i]:
-              
+            if maxarr[i]!=minarr[i]:
                 idx2=i
                 break
-      
         res = idx2-idx1+1
         if res > len(nums):
             return 0 

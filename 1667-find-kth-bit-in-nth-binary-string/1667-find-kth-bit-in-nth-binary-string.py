36a308daa88a74d33ec0e092106a9bf8b452e25c
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-18 09:47:18 -0700

    1667-FIND-KTH-BIT-IN-NTH-BINARY-STRING Stats: Time: 0 ms (100%), Space: 19.3 MB (75.24%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.py
@@ -1,23 +1,18 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        inversion_count = 0
-        
-        while n > 1:
-            length = (1 << n) - 1
-            mid = (length // 2) + 1
-            
-            if k == mid:
-                # Middle bit is "1". Invert if inversion_count is odd.
-                return "0" if inversion_count % 2 == 1 else "1"
+        ic = 0 
+
+        while n>1:
+            length = (1<<n) -1
+            mid = length//2 + 1
+
+            if k ==mid:
+                return "0" if ic%2==1 else "1"
             
             if k > mid:
-                # Mirror k to the left side and track the inversion
-                k = length - k + 1
-                inversion_count += 1
+                k = length-k+1
+                ic+=1
+            n-=1
+        return "1" if ic%2==1 else "0"
             
-            # Reduce problem size to S_{n-1}
-            n -= 1
         
-        # Base case S_1 is always "0". 
-        # Apply accumulated inversions.
-        return "1" if inversion_count % 2 == 1 else "0"

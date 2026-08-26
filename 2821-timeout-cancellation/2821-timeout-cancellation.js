f53af514db3d4ee11adccddf533224e5ec3ee13f
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-26 08:17:13 -0700

    2821-TIMEOUT-CANCELLATION Stats: Time: 62 ms (25.26%), Space: 53.2 MB (91.64%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,45 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+
+
+ // This function sets a timeout and then executes the function f after time t ms but it also provides a trigger to cancel the timeout and the function is the user chooses to invoke it 
+var cancellable = function(fn, args, t) {
+
+    const timerId = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+   
+    return function cancelFn() {
+        clearTimeout(timerId);
+    };
+    
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */

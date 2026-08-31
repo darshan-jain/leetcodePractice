bdac8ea6526e1c63431edc23755c760a39794ab9
Darshan Manoj <darshandhokajain@gmail.com>
2026-08-31 13:28:37 -0700

    2550-WORDS-WITHIN-TWO-EDITS-OF-DICTIONARY Stats: Time: 30 ms (56.73%), Space: 22.2 MB (13.9%) - LeetHub

--- 2550-words-within-two-edits-of-dictionary/2550-words-within-two-edits-of-dictionary.py
+++ 2550-words-within-two-edits-of-dictionary/2550-words-within-two-edits-of-dictionary.py
@@ -1,19 +1,55 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def addword(self, word):
+        d = self.root 
+        for c in word:
+            if c not in d.children:
+                d.children[c] = TrieNode()
+            d = d.children[c]
+        d.isWord = True
+    
+    def search(self, word, root, diff, idx):
+        if diff < 0:
+            return False
+        
+        if idx == len(word):
+            return root.isWord
+
+        letter = word[idx]
+
+        # 1. Option A: Exact match (0 edits)
+        if letter in root.children:
+            if self.search(word, root.children[letter], diff, idx + 1):
+                return True
+
+        # 2. Option B: Substitution edit (1 edit) - try all other branches
+        if diff > 0:
+            for c, child in root.children.items():
+                if c != letter:  # Skip the exact match we already tried above
+                    if self.search(word, child, diff - 1, idx + 1):
+                        return True
+
+        return False
+
+
+
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        dictionary.sort()
-        ans = []
-        def getdist(w1,w2):
-            res = 0 
-            for a,b in zip(w1,w2):
-                if a!=b:
-                    res+=1
-            return res
+        root = Trie()
+
+        for word in dictionary:
+            root.addword(word)
         
+        ans = []
         for word in queries:
-            for ww in dictionary:
-                val = getdist(word, ww)
-                if val<=2:
-                    ans.append(word)
-                    break
+            if root.search(word, root.root,2,0):
+                ans.append(word)
         return ans
+
         

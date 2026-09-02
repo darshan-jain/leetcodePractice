d348b986999c39ed4fe0e6a10d3e626de57e8eac
Darshan Manoj <darshandhokajain@gmail.com>
2026-09-02 06:57:00 -0700

    0657-ROBOT-RETURN-TO-ORIGIN Stats: Time: 14 ms (62.98%), Space: 19.2 MB (87.97%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,9 +1,17 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        moves = list(moves)
-        cnt = Counter(moves)
-        if cnt["L"] == cnt["R"] and cnt["U"]==cnt["D"]:
+        cnt = [0]*4 
+        hm = {
+            "U":0,
+            "D":1,
+            "L":2,
+            "R":3
+        }
+        for c in moves:
+            cnt[hm[c]]+=1
+        
+        if cnt[0]==cnt[1] and cnt[2]==cnt[3]:
             return True
-        else:
-            return False
+        return False
+
         

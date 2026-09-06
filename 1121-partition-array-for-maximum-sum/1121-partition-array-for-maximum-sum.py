03c063ca957877e99b8550a83dc552f61e5bd34d
Darshan Manoj <darshandhokajain@gmail.com>
2026-09-06 07:39:34 -0700

    1121-PARTITION-ARRAY-FOR-MAXIMUM-SUM Stats: Time: 295 ms (16.92%), Space: 20.7 MB (16.7%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.py
@@ -12,11 +12,10 @@ class Solution:
             currmax = 0 
             for j in range(i, min(len(arr),i+k)):
                 currmax = max(currmax, arr[j])
-                windowsize = j-i+1
-                res = max(res, dfs(j+1)+ currmax*windowsize)
+                winsize = j-i+1
+                res = max(res, dfs(j+1)+ currmax*winsize)
                 cache[i] = res
             return res
+
         return dfs(0)
-
-
         

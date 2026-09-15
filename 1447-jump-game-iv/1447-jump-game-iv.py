6bf6b13c5418114d301650b30ac2c883e977042b
Darshan Manoj <darshandhokajain@gmail.com>
2026-09-15 08:49:10 -0700

    1447-JUMP-GAME-IV Stats: Time: 286 ms (14.69%), Space: 33.2 MB (58.6%) - LeetHub

--- 1447-jump-game-iv/1447-jump-game-iv.py
+++ 1447-jump-game-iv/1447-jump-game-iv.py
@@ -1,19 +1,19 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         g = defaultdict(list)
-        for i, x in enumerate(arr):
-            g[x].append(i)
-        q = deque([0])
-        vis = {0}
-        ans = 0
-        while 1:
-            for _ in range(len(q)):
-                i = q.popleft()
-                if i == len(arr) - 1:
-                    return ans
-                for j in (i + 1, i - 1, *g.pop(arr[i], [])):
-                    if 0 <= j < len(arr) and j not in vis:
-                        q.append(j)
-                        vis.add(j)
-            ans += 1
+        for i,num in enumerate(arr):
+            g[num].append(i)
+        q = deque([(0,0)])
+        visit = set()
+        while True:
+            idx, dist = q.popleft()
+            if idx == len(arr)-1:
+                return dist
+            if idx in visit:
+                continue
+            visit.add(idx)
+            for j in (idx-1, idx+1, *g.pop(arr[idx],[])):
+                if 0<=j<len(arr) and j not in visit:
+                    q.append((j ,dist+1))
+        return -1
         

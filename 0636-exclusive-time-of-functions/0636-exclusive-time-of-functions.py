8b7d79d3527016fb74922dee484d208deca649dc
Darshan Manoj <darshandhokajain@gmail.com>
2026-09-17 11:19:49 -0700

    0636-EXCLUSIVE-TIME-OF-FUNCTIONS Stats: Time: 7 ms (60.32%), Space: 19.4 MB (25.17%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,22 +1,19 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: list[str]) -> list[int]:
-        s = []
-        arr = [0]*n
+        stack = []
+        res = [0] * n 
+        prev = 0 
         for log in logs:
-            lst = log.split(":") #id, start/end, timestamp
-            if lst[1]=="start":
-                s.append([int(lst[0]), lst[1], int(lst[2]) ] )
+            log = log.split(":")
+            log[0] = int(log[0])
+            log[2] = int(log[2])
+            if log[1]=="start":
+                if stack:
+                    res[stack[-1]]+=log[2]-prev
+                stack.append(log[0])
+                prev = log[2]
             else:
-                # same id or diff id 
-                if int(lst[0])==s[-1][0]:
-                    time = int(lst[2]) - s[-1][2] + 1
-                    arr[s[-1][0]]+=time
-                    s.pop()
-                    if s:
-                        s[-1][2]+=time
-                
-                # else: #mostly shouldn't go to this case
-                #     s.append((int(lst[0]), lst[1], int(lst[2]) ) )
-        return arr
-
+                res[stack.pop()]+=log[2] - prev+1
+                prev = log[2]+1
+        return res
         
